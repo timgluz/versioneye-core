b272ce96600b1f8ea0a360b78c3eca5fc0cdfaac
reiz <robert.reiz.81@gmail.com>
2014-03-28 20:56:43 +0100

    Add user mailer and tests

--- lib/versioneye/mailers/user_mailer.rb
+++ lib/versioneye/mailers/user_mailer.rb
@@ -7,106 +7,64 @@ class UserMailer < ActionMailer::Base
     @user = user
     @plan = user.plan
     @billing_address = user.billing_address
-    mail(
-      :to => user.email,
-      :subject => 'Receipt',
-      :tag => 'receipt'
-      )
+    mail(:to => user.email, :subject => 'Receipt')
   end
 
   def verification_email(user, verification, email)
-    @user = user
+    @user  = user
     source = fetch_source( user )
-    @verificationlink = "#{Settings.server_url_https}/users/activate/#{source}/#{verification}"
-    mail(
-      :to => email,
-      :subject => 'Verification',
-      :tag => 'verification'
-      )
+    @verificationlink = "#{Settings.instance.server_url_https}/users/activate/#{source}/#{verification}"
+    mail( :to => email, :subject => 'Verification' )
   end
 
   def verification_email_only(user, verification, email)
     @user = user
-    @verificationlink = "#{Settings.server_url_https}/users/activate/email/#{verification}"
-    mail(
-      :to => email,
-      :subject => 'Verification',
-      :tag => 'verification'
-      )
+    @verificationlink = "#{Settings.instance.server_url_https}/users/activate/email/#{verification}"
+    mail(:to => email, :subject => 'Verification')
   end
 
   def verification_email_reminder(user, verification, email)
-    @user = user
+    @user  = user
     source = fetch_source( user )
-    @verificationlink = "#{Settings.server_url_https}/users/activate/#{source}/#{verification}"
-    mail(
-      :to => email,
-      :subject => 'Verification Reminder',
-      :tag => 'verification_reminder'
-      )
+    @verificationlink = "#{Settings.instance.server_url_https}/users/activate/#{source}/#{verification}"
+    mail( :to => email, :subject => 'Verification Reminder' )
   end
 
   def collaboration_invitation(collaborator)
-    @caller = collaborator.caller
-    @owner = collaborator.owner
+    @caller  = collaborator.caller
+    @owner   = collaborator.owner
     @project = collaborator.project
-
-    mail(
-      :to => collaborator[:invitation_email],
-      :subject => 'Invitation to project collabration',
-      :tag => 'collaboration'
-    )
+    mail( :to => collaborator[:invitation_email], :subject => 'Invitation to project collabration' )
   end
 
   def new_collaboration(collaborator)
-    @caller = collaborator.caller
-    @project = collaborator.project
-    @callee = collaborator.user
+    @caller        = collaborator.caller
+    @project       = collaborator.project
+    @callee        = collaborator.user
     @collaboration = collaborator
-
-    mail(
-      :to => @callee[:email],
-      :subject => "#{@caller[:fullname]} added you as collaborator.",
-      :tag => 'collaboration'
-    )
+    mail( :to => @callee[:email], :subject => "#{@caller[:fullname]} added you as collaborator" )
   end
 
   def reset_password(user)
     @user = user
-    @url = "#{Settings.server_url_https}/updatepassword/#{@user.verification}"
-    mail(
-      :to => @user.email,
-      :subject => 'Password Reset',
-      :tag => 'password_reset'
-      )
+    @url  = "#{Settings.instance.server_url_https}/updatepassword/#{@user.verification}"
+    mail(:to => @user.email, :subject => 'Password Reset')
   end
 
   def new_user_email(user)
     @user = user
-    mail(
-      :to => "reiz@versioneye.com",
-      :subject => "New User",
-      :tag => "new_user"
-      )
+    mail( :to => "reiz@versioneye.com", :subject => "New User" )
   end
 
   def new_ticket(user, ticket)
     @fullname = user[:fullname]
-    @ticket = ticket
-    mail(
-      :to => user[:email],
-      :subject => "VersionEye's lottery confirmation",
-      :tag => "new_lottery"
-    )
+    @ticket   = ticket
+    mail(:to => user[:email], :subject => "VersionEye's lottery confirmation")
   end
 
   def suggest_packages_email( user )
     @fullname = user[:fullname]
-    mail(
-      :to => user[:email],
-      :subject => "Follow popular software packages on VersionEye",
-      :tag => "suggest_packages"
-    )
+    mail(:to => user[:email], :subject => "Follow popular software packages on VersionEye")
   end
 
   def fetch_source( user )
---
+++ spec/versioneye/mailers/user_mailer_spec.rb
@@ -0,0 +1,229 @@
+require 'spec_helper'
+
+describe UserMailer do
+
+  describe 'verification_email' do
+
+    it 'should contain the verification link' do
+
+      user = UserFactory.create_new
+      verification = "aslaksasasas8888asg8ag"
+
+      email = described_class.verification_email( user, verification, user.email )
+
+      email.to.should eq( [user.email] )
+      email.encoded.should include( "Hello #{user.fullname}" )
+      email.encoded.should include( "#{Settings.instance.server_url_https}/users/activate/" )
+      email.encoded.should include( "#{verification}" )
+      email.encoded.should include( "Handelsregister" )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'verification_email_only' do
+
+    it 'should contain the verification link' do
+
+      user = UserFactory.create_new
+      verification = "aslaksasasas8888asg8ag"
+
+      email = described_class.verification_email_only( user, verification, user.email )
+
+      email.to.should eq( [user.email] )
+      email.encoded.should include( "Hello #{user.fullname}" )
+      email.encoded.should include( "#{Settings.instance.server_url_https}/users/activate/email/#{verification}" )
+      email.encoded.should include( "Handelsregister" )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'verification_email_reminder' do
+
+    it 'should contain the verification link and a reminder' do
+
+      user = UserFactory.create_new
+      verification = "aslaksasasas8888asg8ag"
+
+      email = described_class.verification_email_reminder( user, verification, user.email )
+
+      email.to.should eq( [user.email] )
+      email.subject.should eq('Verification Reminder')
+      email.encoded.should include( "Hello #{user.fullname}" )
+      email.encoded.should include( "#{Settings.instance.server_url_https}/users/activate/" )
+      email.encoded.should include( "#{verification}" )
+      email.encoded.should include( "We noticed that you still didn't activate your account." )
+      email.encoded.should include( "Handelsregister" )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'collaboration_invitation' do
+
+    it 'should contain the message for the new collaborator' do
+
+      owner        = UserFactory.create_new
+      project      = ProjectFactory.create_new owner
+      project_col  = ProjectCollaborator.new({:owner_id => owner.id.to_s, :caller_id => owner.id.to_s })
+      project_col.project = project
+      project_col.invitation_email = 'test@test.de'
+      project.save
+
+      email = described_class.collaboration_invitation( project_col )
+
+      email.to.should eq( ['test@test.de'] )
+      email.subject.should eq('Invitation to project collabration')
+      email.encoded.should include( "Hey Dude" )
+      email.encoded.should include( "#{owner.fullname} invites you" )
+      email.encoded.should include( "#{project.name}" )
+      email.encoded.should include( "Handelsregister" )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'new_collaboration' do
+
+    it 'should contain the verification link' do
+
+      owner        = UserFactory.create_new 1
+      collaborator = UserFactory.create_new 2
+      project      = ProjectFactory.create_new owner
+      project_col  = ProjectCollaborator.new({:user_id => collaborator.id.to_s, :owner_id => owner.id.to_s, :caller_id => owner.id.to_s })
+      project_col.project = project
+      project.save
+
+      email = described_class.new_collaboration( project_col )
+
+      email.to.should eq( [collaborator.email] )
+      email.subject.should eq("#{project_col.caller.fullname} added you as collaborator")
+      email.encoded.should include( "Hey #{collaborator.fullname}" )
+      email.encoded.should include( "#{owner.fullname} added you as collaborator" )
+      email.encoded.should include( "#{project.name}" )
+      email.encoded.should include( 'user/collaborations' )
+      email.encoded.should include( 'Handelsregister' )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'reset_password' do
+
+    it 'contain link to update the password' do
+
+      user = UserFactory.create_new 1
+      user.verification = 'asfgalsfgasgj988asg87asg'
+      user.save
+
+      email = described_class.reset_password( user )
+
+      email.to.should eq( [user.email] )
+      email.subject.should eq('Password Reset')
+      email.encoded.should include( "You've requested a new password" )
+      email.encoded.should include( "#{Settings.instance.server_url_https}/updatepassword/#{user.verification}" )
+      email.encoded.should include( 'Handelsregister' )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'new_user_email' do
+
+    it 'contain the user data from the new sign up' do
+
+      user = UserFactory.create_new 1
+
+      email = described_class.new_user_email( user )
+
+      email.to.should eq( ['reiz@versioneye.com'] )
+      email.subject.should eq('New User')
+      email.encoded.should include( "Hey Admin Dude" )
+      email.encoded.should include( "#{user.fullname}" )
+      email.encoded.should include( "#{user.username}" )
+      email.encoded.should include( "#{user.github_id}" )
+      email.encoded.should include( "#{user.bitbucket_id}" )
+      email.encoded.should include( 'Handelsregister' )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'new_ticket' do
+
+    it 'contains the new lottery ticket' do
+
+      user = UserFactory.create_new 1
+      prod_1 = ProductFactory.create_new 1
+      prod_2 = ProductFactory.create_new 2
+      prod_3 = ProductFactory.create_new 3
+      ticket = Lottery.new({:user_id => user.id.to_s })
+      ticket.selection = []
+      ticket.selection << {language: prod_1.language, prod_key: prod_1.prod_key}
+      ticket.selection << {language: prod_2.language, prod_key: prod_2.prod_key}
+      ticket.selection << {language: prod_3.language, prod_key: prod_3.prod_key}
+
+      email = described_class.new_ticket( user, ticket )
+
+      email.to.should eq( [user.email] )
+      email.subject.should eq("VersionEye's lottery confirmation")
+      email.encoded.should include( "Hello #{user.fullname}" )
+      email.encoded.should include( "Here's your ticket id #{ticket[:_id].to_s}" )
+      email.encoded.should include( prod_1.to_param )
+      email.encoded.should include( prod_2.to_param )
+      email.encoded.should include( prod_3.to_param )
+      email.encoded.should include( 'Handelsregister' )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+  describe 'suggest_packages_email' do
+
+    it 'contains suggestions to follow' do
+
+      user = UserFactory.create_new 1
+
+      email = described_class.suggest_packages_email( user )
+
+      email.to.should eq( [user.email] )
+      email.subject.should eq("Follow popular software packages on VersionEye")
+      email.encoded.should include( "Hello #{user.fullname}" )
+      email.encoded.should include( "we noticed you signed up for VersionEye.com but didn't follow any libraries yet" )
+      email.encoded.should include( "ruby" )
+      email.encoded.should include( 'Handelsregister' )
+
+      ActionMailer::Base.deliveries.clear
+      email.deliver!
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+  end
+
+end
+
