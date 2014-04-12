4e17556fb79f7080858c00b5a57cead6b9c60819
reiz <robert.reiz.81@gmail.com>
2014-04-12 21:42:55 +0200

    Refactor DependencyFactory

--- spec/versioneye/domain_factories/dependency_factory.rb
+++ spec/versioneye/domain_factories/dependency_factory.rb
@@ -2,14 +2,17 @@ class DependencyFactory
 
   def self.create_new(product, dep_product, store = true)
     dependency              = Dependency.new
+
+    dependency.prod_key     = product.prod_key
+    dependency.prod_version = product.version
+
     dependency.name         = dep_product.name
     dependency.version      = dep_product.version
     dependency.dep_prod_key = dep_product.prod_key
-    dependency.scope        = Dependency::A_SCOPE_COMPILE
-    dependency.prod_key     = product.prod_key
-    dependency.prod_version = product.version
     dependency.prod_type    = dep_product.prod_type
     dependency.language     = dep_product.language
+    dependency.scope        = Dependency::A_SCOPE_COMPILE
+
     if store
       dependency.save
     end
