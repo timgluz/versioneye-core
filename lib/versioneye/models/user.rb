5a74c54b69b63fe1269f8a665db5907d46863a1f
reiz <robert.reiz.81@gmail.com>
2014-04-17 15:44:04 +0200

    Change email regex.

--- lib/versioneye/models/user.rb
+++ lib/versioneye/models/user.rb
@@ -1,6 +1,6 @@
 class User < Versioneye::Model
 
-  A_EMAIL_REGEX = /\A([\w\!\#\z\%\&\'\*\+\-\/\=\?\\A\`{\|\}\~]+\.)*[\w\+-]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)\z/i
+  A_EMAIL_REGEX = /[\S]+\@[\S]+\.[\S]+\z/i
 
   include Mongoid::Document
   include Mongoid::Timestamps
