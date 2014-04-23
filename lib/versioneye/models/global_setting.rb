dd97c2bebd7f6dca498d451f965d5d1970e6c39b
reiz <robert.reiz.81@gmail.com>
2014-04-23 16:39:02 +0200

    Add server host/port to global settings

--- lib/versioneye/models/global_setting.rb
+++ lib/versioneye/models/global_setting.rb
@@ -3,7 +3,9 @@ class GlobalSetting < Versioneye::Model
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :server_url, type: String , default: 'http://localhost:3000'
+  field :server_url , type: String , default: 'http://localhost:3000'
+  field :server_host, type: String , default: 'localhost'
+  field :server_port, type: Integer, default: 3000
 
   validates_presence_of :server_url , :message => 'is mandatory!'
 
