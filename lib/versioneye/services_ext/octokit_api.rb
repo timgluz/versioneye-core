7beccfb9fcf9fa9d3092c773d75c879c412f8538
reiz <robert.reiz.81@gmail.com>
2014-05-11 11:13:34 +0200

    Cleanup OctokitApi

--- lib/versioneye/services_ext/octokit_api.rb
+++ lib/versioneye/services_ext/octokit_api.rb
@@ -3,11 +3,18 @@ require 'octokit'
 class OctokitApi
 
   def self.application_authentication
-    { :client_id => Settings.instance.github_client_id, :client_secret => Settings.instance.github_client_secret, :api_endpoint => Settings.instance.github_api_url }
+    {
+      :client_id     => Settings.instance.github_client_id,
+      :client_secret => Settings.instance.github_client_secret,
+      :api_endpoint  => Settings.instance.github_api_url
+    }
   end
 
   def self.token_authentication token
-    { :access_token => token, :api_endpoint => Settings.instance.github_api_url }
+    {
+      :access_token => token,
+      :api_endpoint => Settings.instance.github_api_url
+    }
   end
 
   def self.client token = nil
