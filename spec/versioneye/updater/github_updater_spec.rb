6c25752beb232de992dd7b97e3f284722b4daf21
reiz <robert.reiz.81@gmail.com>
2014-05-12 14:11:38 +0200

    Add VCR to github_updater_spec

--- spec/versioneye/updater/github_updater_spec.rb
+++ spec/versioneye/updater/github_updater_spec.rb
@@ -13,15 +13,18 @@ describe GithubUpdater do
     end
 
     it 'returns the updated project' do
+
       user = UserFactory.create_new
       project = ProjectFactory.default user
       project.s3_filename = 'pom.xml'
       project.scm_fullname = 'versioneye/versioneye_maven_plugin'
       project.scm_branch = 'master'
 
-      described_class.new.update project
-      project.should_not be_nil
-      project.dependencies.count.should == 11
+      VCR.use_cassette('github_updater_pom_xml_1', allow_playback_repeats: true) do
+        described_class.new.update project
+        project.should_not be_nil
+        project.dependencies.count.should == 11
+      end
     end
 
     it 'returns the updated project / Podfile' do
@@ -31,9 +34,11 @@ describe GithubUpdater do
       project.scm_fullname = 'xing/XNGAPIClient'
       project.scm_branch = 'master'
 
-      described_class.new.update project
-      project.should_not be_nil
-      project.dependencies.count.should == 4
+      VCR.use_cassette('github_updater_podfile_1', allow_playback_repeats: true) do
+        described_class.new.update project
+        project.should_not be_nil
+        project.dependencies.count.should == 4
+      end
     end
 
   end
@@ -47,11 +52,13 @@ describe GithubUpdater do
       project.scm_fullname = 'versioneye/versioneye_maven_plugin'
       project.scm_branch = 'master'
 
-      pf = described_class.new.fetch_project_file project
-      pf.should_not be_nil
-      pf[:type].should eq("Maven2")
-      pf[:name].should eq("pom.xml")
-      pf[:content].should_not be_nil
+      VCR.use_cassette('github_updater_pom_xml_2', allow_playback_repeats: true) do
+        pf = described_class.new.fetch_project_file project
+        pf.should_not be_nil
+        pf[:type].should eq("Maven2")
+        pf[:name].should eq("pom.xml")
+        pf[:content].should_not be_nil
+      end
     end
 
   end
