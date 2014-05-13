c152c282b2d64922e0484c538a3c3d9c3de35561
reiz <robert.reiz.81@gmail.com>
2014-05-13 18:26:43 +0200

    Remove unused code

--- lib/versioneye/services_ext/s3.rb
+++ lib/versioneye/services_ext/s3.rb
@@ -59,40 +59,6 @@ class S3 < Versioneye::Service
   end
 
 
-  def self.upload_github_file file, filename
-    return nil if file.nil? || filename.to_s.empty?
-    file_bin     = file[:content]
-    random_value = Project.create_random_value
-    new_filename = "#{random_value}_#{filename}"
-    self.store_in_project_bucket new_filename, Base64.decode64(file_bin)
-    url                = S3.url_for( new_filename )
-    result             = Hash.new
-    result['filename'] = new_filename
-    result['s3_url']   = url
-    result
-  rescue => e
-    log.error e.message
-    log.error e.backtrace.join '\n'
-  end
-
-
-  def self.upload_file_content( content, filename)
-    return nil if content.nil? || filename.to_s.empty?
-    file_bin = content
-    random_value = Project.create_random_value
-    new_filename = "#{random_value}_#{filename}"
-    self.store_in_project_bucket new_filename, file_bin #Base64.decode64(file_bin)
-    url                = S3.url_for( new_filename )
-    result             = Hash.new
-    result['filename'] = new_filename
-    result['s3_url']   = url
-    result
-  rescue => e
-    log.error e.message
-    log.error e.backtrace.join '\n'
-  end
-
-
   def self.store_in_project_bucket filename, bin
     bucket = AWS.s3.buckets[Settings.instance.s3_projects_bucket]
     obj    = bucket.objects[ filename ]
