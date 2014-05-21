3b91e9b8400784c192a634a387c5a6a3ebdd559e
reiz <robert.reiz.81@gmail.com>
2014-05-21 14:37:09 +0200

    Add random number to feedback mail subject

--- lib/versioneye/mailers/feedback_mailer.rb
+++ lib/versioneye/mailers/feedback_mailer.rb
@@ -7,9 +7,20 @@ class FeedbackMailer < ActionMailer::Base
     @name     = name
     @email    = email
     @feedback = feedback
-    mail(:to => 'reiz@versioneye.com', :subject => 'VersionEye Feedback') do |format|
+    rnd_val = create_random_value
+    subject = "VersionEye Feedback #{rnd_val}"
+    mail(:to => 'reiz@versioneye.com', :subject => subject) do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
+
+  private
+
+    def create_random_value
+      chars = '0123456789'
+      value = ""
+      7.times { value << chars[rand(chars.size)] }
+      value
+    end
 
 end
