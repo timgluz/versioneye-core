7a1f5127b3b747305b2985c6d61524f1b76d83b2
reiz <robert.reiz.81@gmail.com>
2014-05-30 12:36:09 +0200

    Change plan names

--- lib/versioneye/models/plan.rb
+++ lib/versioneye/models/plan.rb
@@ -25,7 +25,7 @@ class Plan < Versioneye::Model
     Plan.where(:name_id => name_id).shift
   end
 
-  def self.create_default_plans
+  def self.create_defaults
     trial_0 = Plan.new
     trial_0.name_id = A_PLAN_TRIAL_0
     trial_0.name = 'Trial / Free'
@@ -35,49 +35,49 @@ class Plan < Versioneye::Model
 
     trial_1 = Plan.new
     trial_1.name_id = A_PLAN_TRIAL_1
-    trial_1.name = 'Trial / 1'
+    trial_1.name = 'Trial / Starter'
     trial_1.price = '1'
     trial_1.private_projects = 2
     trial_1.save
 
     personal_1 = Plan.new
     personal_1.name_id = A_PLAN_PERSONAL_3
-    personal_1.name = 'Personal / 3'
+    personal_1.name = 'Personal / Small'
     personal_1.price = '3'
     personal_1.private_projects = 5
     personal_1.save
 
     personal_2 = Plan.new
     personal_2.name_id = A_PLAN_PERSONAL_6
-    personal_2.name = 'Personal / 6'
+    personal_2.name = 'Personal / Normal'
     personal_2.price = '6'
     personal_2.private_projects = 10
     personal_2.save
 
     personal_3 = Plan.new
     personal_3.name_id = A_PLAN_PERSONAL_9
-    personal_3.name = 'Personal / 9'
+    personal_3.name = 'Personal / Big'
     personal_3.price = '9'
     personal_3.private_projects = 15
     personal_3.save
 
     business_1 = Plan.new
     business_1.name_id = A_PLAN_BUSINESS_25
-    business_1.name = 'Business / 25'
+    business_1.name = 'Business / Small'
     business_1.price = '25'
     business_1.private_projects = 50
     business_1.save
 
     business_2 = Plan.new
     business_2.name_id = A_PLAN_BUSINESS_50
-    business_2.name = 'Business / 50'
+    business_2.name = 'Business / Normal'
     business_2.price = '50'
     business_2.private_projects = 100
     business_2.save
 
     business_3 = Plan.new
     business_3.name_id = A_PLAN_BUSINESS_100
-    business_3.name = 'Business / 100'
+    business_3.name = 'Business / Big'
     business_3.price = '100'
     business_3.private_projects = 200
     business_3.save
