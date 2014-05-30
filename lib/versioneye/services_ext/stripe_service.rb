4c0c5fb0835dfcaf3719a58b890f40b74d0a4ffe
reiz <robert.reiz.81@gmail.com>
2014-05-30 14:23:53 +0200

    Refactor StripeService

--- lib/versioneye/services_ext/stripe_service.rb
+++ lib/versioneye/services_ext/stripe_service.rb
@@ -1,8 +1,10 @@
 class StripeService < Versioneye::Service
 
+
   def self.fetch_customer customer_id
     Stripe::Customer.retrieve customer_id
   end
+
 
   def self.create_customer stripe_token, plan_name_id, email
     Stripe::Customer.create(
@@ -12,24 +14,32 @@ class StripeService < Versioneye::Service
       )
   end
 
+
+  def self.update_customer user, stripe_token, plan_name_id
+    customer = self.fetch_customer user.stripe_customer_id
+    customer.card = stripe_token
+    customer.save
+    customer.update_subscription( :plan => plan_name_id )
+    customer
+  end
+
+
   def self.create_or_update_customer user, stripe_token, plan_name_id
     if user.stripe_customer_id
-      customer = self.fetch_customer user.stripe_customer_id
-      customer.card = stripe_token
-      customer.save
-      customer.update_subscription( :plan => plan_name_id )
-      return customer
+      return self.update_customer user, stripe_token, plan_name_id
     end
     self.create_customer stripe_token, plan_name_id, user.email
   rescue => e
     log.error e.message
-    log.error e.backtrace.join("\n")
+    log.error e.backtrace.join('\n')
     nil
   end
+
 
   def self.get_invoice invoice_id
     Stripe::Invoice.retrieve invoice_id
   end
+
 
   def self.delete customer_id
     return nil if customer_id.to_s.empty?
@@ -37,7 +47,7 @@ class StripeService < Versioneye::Service
     customer.delete
   rescue => e
     log.error e.message
-    log.error e.backtrace.join("\n")
+    log.error e.backtrace.join('\n')
   end
 
 end
