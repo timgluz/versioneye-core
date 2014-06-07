70c1e2a3fc1808c86500738a87c840b7c26a9c40
reiz <robert.reiz.81@gmail.com>
2014-06-07 17:15:51 +0200

    more tests for update strategy

--- spec/versioneye/updater/update_strategy_spec.rb
+++ spec/versioneye/updater/update_strategy_spec.rb
@@ -8,14 +8,27 @@ describe UpdateStrategy do
       updater = UpdateStrategy.updater_for Project::A_SOURCE_URL
       updater.instance_of?(UrlUpdater).should be_true
     end
+
+    it 'returns the Upload Updater' do
+      updater = UpdateStrategy.updater_for Project::A_SOURCE_API
+      updater.instance_of?(UrlUpdater).should be_true
+    end
+
+    it 'returns the Upload Updater' do
+      updater = UpdateStrategy.updater_for 'nil'
+      updater.instance_of?(UrlUpdater).should be_true
+    end
+
     it 'returns the Upload Updater' do
       updater = UpdateStrategy.updater_for Project::A_SOURCE_UPLOAD
       updater.instance_of?(UploadUpdater).should be_true
     end
+
     it 'returns the GitHub Updater' do
       updater = UpdateStrategy.updater_for Project::A_SOURCE_GITHUB
       updater.instance_of?(GithubUpdater).should be_true
     end
+
     it 'returns the Bitbucket Updater' do
       updater = UpdateStrategy.updater_for Project::A_SOURCE_BITBUCKET
       updater.instance_of?(BitbucketUpdater).should be_true
