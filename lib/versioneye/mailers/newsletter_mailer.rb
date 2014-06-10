b7dc6cf7badf7cb84511c1575415fc48b09e5a48
reiz <robert.reiz.81@gmail.com>
2014-06-10 15:36:04 +0200

    New newsletter

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -5,7 +5,7 @@ class NewsletterMailer < ActionMailer::Base
 
   def newsletter_new_features_email(user)
     @user = user
-    mail(:to => @user.email, :subject => 'Big Spring Refactoring') do |format|
+    mail(:to => @user.email, :subject => 'New Pricing') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
