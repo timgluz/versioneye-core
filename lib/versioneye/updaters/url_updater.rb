dcc6f27aae6a2500354cb74c6bc5bb7cb2cb3dd0
reiz <robert.reiz.81@gmail.com>
2014-07-15 08:58:14 +0200

    Log error if parser is not available

--- lib/versioneye/updaters/url_updater.rb
+++ lib/versioneye/updaters/url_updater.rb
@@ -2,8 +2,16 @@ class UrlUpdater < CommonUpdater
 
   def update( project, send_email = false )
     parser = parser_for project.url
+    if parser.nil?
+      log.error "No parser found for project (#{project.id}) url: #{project.url}"
+      return
+    end
+
     new_project = parser.parse project.url
     update_old_with_new project, new_project, send_email
+  rescue => e
+    User.log.error e.message
+    User.log.error e.backtrace.join('\n')
   end
 
 end
