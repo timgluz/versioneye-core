5f819b530fdd5c7ab6f4cf7e23b26d1dfc126a28
reiz <robert.reiz.81@gmail.com>
2014-09-09 09:40:16 +0200

    Improve License Search

--- lib/versioneye/services/license_service.rb
+++ lib/versioneye/services/license_service.rb
@@ -1,10 +1,17 @@
 class LicenseService < Versioneye::Service
 
   def self.search name
-    SpdxLicense.where(:fullname => /#{name}/i)
+    result1 = SpdxLicense.where(:fullname => /#{name}/i)
+    result2 = SpdxLicense.where(:identifier => /#{name}/i)
+    keys = []
+    results = []
+    result1.map{|lic| results << lic; keys << lic.identifier } if result1 && !result1.empty?
+    result2.map{|lic| results << lic if !keys.include?(lic.identifier) } if result2 && !result2.empty?
+    results
   rescue => e
     log.error e.message
     log.error e.backtrace.join("\n")
+    []
   end
 
   def self.import_from file_path
@@ -23,5 +30,7 @@ class LicenseService < Versioneye::Service
     rescue => e
       p e.message
     end
+
+
 
 end
