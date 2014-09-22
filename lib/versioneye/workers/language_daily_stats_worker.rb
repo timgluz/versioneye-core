511f4ab222965b573810a41ab9adef50e3ac99b8
reiz <robert.reiz.81@gmail.com>
2014-09-22 20:49:32 +0200

    Handle LanguageDailyStatus through Rabbit

---
+++ lib/versioneye/workers/language_daily_stats_worker.rb
@@ -0,0 +1,40 @@
+class LanguageDailyStatsWorker < Worker
+
+
+  def work
+    connection = get_connection
+    connection.start
+    channel = connection.create_channel
+    queue   = channel.queue("language_daily_stats", :durable => true)
+
+    log_msg = " [*] Waiting for messages in #{queue.name}. To exit press CTRL+C"
+    puts log_msg
+    log.info log_msg
+
+    begin
+      queue.subscribe(:ack => true, :block => true) do |delivery_info, properties, body|
+        msg = " [x] Received #{body}"
+        puts msg
+        log.info msg
+
+        update_counts
+
+        channel.ack(delivery_info.delivery_tag)
+      end
+    rescue => e
+      log.error e.message
+      log.error e.backtrace.join("\n")
+      connection.close
+    end
+  end
+
+
+  def update_counts
+    LanguageDailyStats.update_counts(3, 1)
+  rescue => e
+    log.error e.message
+    log.error e.backtrace.join("\n")
+  end
+
+
+end
