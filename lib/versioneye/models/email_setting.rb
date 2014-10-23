078bfb2060ca27d5bc4540c52556daaf6b945e6a
reiz <robert.reiz.81@gmail.com>
2014-10-23 09:20:13 +0200

    Update gemspec

--- lib/versioneye/models/email_setting.rb
+++ lib/versioneye/models/email_setting.rb
@@ -24,4 +24,24 @@ class EmailSetting < Versioneye::Model
     email_setting
   end
 
+  def update_from params
+    self.address              = params[:address]
+    self.port                 = params[:port]
+    self.username             = params[:username]
+    self.password             = params[:password] if params[:password]
+    self.domain               = params[:domain]
+    self.authentication       = params[:authentication]
+    tls = false
+    if params[:enable_starttls_auto] && params[:enable_starttls_auto].eql?("true")
+      tls = true
+    end
+    self.enable_starttls_auto = tls
+    self.sender_name          = params[:sender_name] if params[:sender_name]
+    self.sender_email         = params[:sender_email] if params[:sender_email]
+    self.save
+  rescue => e
+    log.error e.message
+    false
+  end
+
 end
