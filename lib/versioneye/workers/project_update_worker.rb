354fc4ca77ca8b8045cbbaf2f5655f78732b4b2e
reiz <robert.reiz.81@gmail.com>
2014-12-02 17:58:24 +0100

    Add comment

--- lib/versioneye/workers/project_update_worker.rb
+++ lib/versioneye/workers/project_update_worker.rb
@@ -1,5 +1,7 @@
 class ProjectUpdateWorker < Worker
 
+# This worker handels the period update jobs (DAILY, WEEKLY, MONTHLY)
+# And the single project updates. 
 
   def work
     connection = get_connection
