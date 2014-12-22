2e98c659645cbb5421bbcb55e9c00c4c57e54a89
reiz <robert.reiz.81@gmail.com>
2014-12-22 18:00:25 +0100

    Update project sums

--- lib/versioneye/models/project.rb
+++ lib/versioneye/models/project.rb
@@ -271,6 +271,7 @@ class Project < Versioneye::Model
     self.unknown_number_sum   = self.unknown_number
     self.licenses_red_sum     = self.licenses_red
     self.licenses_unknown_sum = self.licenses_unknown
+    self.save 
   end
 
   def sum_reset!
@@ -279,6 +280,7 @@ class Project < Versioneye::Model
     self.unknown_number_sum   = 0
     self.licenses_red_sum     = 0
     self.licenses_unknown_sum = 0
+    self.save 
   end
 
   private
