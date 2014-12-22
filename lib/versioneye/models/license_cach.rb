e4beb2a6834fb1f376b69d43aa13f9c0dfe916e1
reiz <robert.reiz.81@gmail.com>
2014-12-22 12:08:22 +0100

    Add more documentation to license_cach

--- lib/versioneye/models/license_cach.rb
+++ lib/versioneye/models/license_cach.rb
@@ -5,6 +5,10 @@ class LicenseCach < Versioneye::Model
 
   field :name      , type: String # For example MIT
   field :url       , type: String # URL to the license text
+
+  # true  = it's conform with the license whitelist of the project
+  # false = it violates the license whitelist of the project 
+  # nil   = There is no license white list defined in the project 
   field :on_whitelist, type: Boolean
   
   field :license_id, type: String 
