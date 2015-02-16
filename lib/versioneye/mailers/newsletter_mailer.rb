f0193d25355622bb1eb54c361895338d13f80e7f
reiz <robert.reiz.81@gmail.com>
2015-02-16 14:10:47 +0100

    Update Newsletter!

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -1,13 +1,16 @@
 class NewsletterMailer < ActionMailer::Base
 
+  
   layout 'email_html_layout'
   default from: "\"#{Settings.instance.smtp_sender_name}\" <#{Settings.instance.smtp_sender_email}>"
 
-  def newsletter_new_features_email(user)
+  
+  def newsletter_new_features_email( user )
     @user = user
-    mail(:to => @user.email, :subject => 'Mary Christmas - Multiple Files per Project!') do |format|
+    mail(:to => @user.email, :subject => 'Public Ticket System for VersionEye!') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
+
 
 end
