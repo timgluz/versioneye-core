5bdee573524e3b706af0077c1beb1415a47d65c0
reiz <robert.reiz.81@gmail.com>
2015-02-26 13:44:37 +0100

    Update gemspec

--- lib/versioneye/models/enterprise_lead.rb
+++ lib/versioneye/models/enterprise_lead.rb
@@ -3,20 +3,19 @@ class EnterpriseLead < Versioneye::Model
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :scm           , type: String, default: 'GitHub Enterprise'
-  field :repository    , type: String, default: 'Nexus'
-  field :ci            , type: String, default: 'Jenkins'
-  field :virtualization, type: String, default: 'VMWare'
+  field :scm           , type: String, default: 'github'
+  field :repository    , type: String, default: 'nexus'
+  field :ci            , type: String, default: 'jenkins'
+  field :virtualization, type: String, default: 'vmware'
   field :name          , type: String
-  field :email         , type: String , default: 'notify@versioneye.com'
+  field :email         , type: String
   field :note          , type: String
 
-  validates_presence_of :scm,            :message => 'is mandatory!'
-  validates_presence_of :repository,     :message => 'is mandatory!'
-  validates_presence_of :ci,             :message => 'is mandatory!'
-  validates_presence_of :virtualization, :message => 'is mandatory!'
-  validates_presence_of :name,           :message => 'is mandatory!'
-  validates_presence_of :email,          :message => 'is mandatory!'
-  validates_presence_of :note,           :message => 'is mandatory!'
+  validates_presence_of :scm,            :message => 'is mandatory'
+  validates_presence_of :repository,     :message => 'is mandatory'
+  validates_presence_of :ci,             :message => 'is mandatory'
+  validates_presence_of :virtualization, :message => 'is mandatory'
+  validates_presence_of :name,           :message => 'is mandatory'
+  validates_presence_of :email,          :message => 'is mandatory'
 
 end
