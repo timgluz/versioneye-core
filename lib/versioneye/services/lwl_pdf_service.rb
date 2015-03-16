b2b0e7c8e78107fbf553bfd8ff6cd9e1bd3af1aa
reiz <robert.reiz.81@gmail.com>
2015-03-16 10:13:50 +0100

    Refactor LwlPdfService!

--- lib/versioneye/services/lwl_pdf_service.rb
+++ lib/versioneye/services/lwl_pdf_service.rb
@@ -3,39 +3,40 @@ class LwlPdfService < Versioneye::Service
   require 'pdfkit'
 
   
-  def self.process project 
+  def self.process project, write_to_disk = false
     fill_dto project 
     html = compile_html project
-    pdf  = compile_pdf html, project 
+    kit  = new_kit html
+    
+    write_pdf_to_disk(kit, project) if write_to_disk
+
+    kit.to_pdf
   end
 
 
   def self.compile_html project
-    # content_file = Settings.instance.receipt_content
-    content_file = 'lib/versioneye/views/lwl_pdf/lwl_pdf.html.erb'
-
+    content_file = Settings.instance.lwl_pdf_content
     erb = ERB.new(File.read(content_file))
     html = erb.result(project.get_binding)
     html = html.force_encoding(Encoding::UTF_8)
-
     html
   end
 
 
-  # Note for me.. kit.to_file('/Users/robertreiz/invoice.pdf')
-  def self.compile_pdf html, project = nil
-    # footer_file = Settings.instance.receipt_footer
-    footer_file = 'lib/versioneye/views/lwl_pdf/footer.html'
+  def self.new_kit html
+    footer_file = Settings.instance.lwl_pdf_footer
     kit = PDFKit.new(html, :footer_html => footer_file, :page_size => 'A4')
 
     raise "PDFKit.new returned nil!" if kit.nil?
 
-    if project 
-      project_name = project.name.gsub("/", "-")
-      kit.to_file("#{ENV['HOME']}/#{project_name}.pdf")
-    end
+    kit 
+  end
 
-    kit.to_pdf
+
+  def self.write_pdf_to_disk kit, project
+    date_string = DateTime.now.strftime("%d_%m_%Y")
+    project_name = project.name.gsub("/", "-")
+    kit.to_file("#{ENV['HOME']}/#{date_string}_#{project_name}.pdf")  
   end
 
 
@@ -63,7 +64,9 @@ class LwlPdfService < Versioneye::Service
 
 
     def self.build_line dep, name 
-      {:component => dep.name, :version => dep.version_requested, :license => name}
+      dep_name = dep.name 
+      dep_name = dep.artifact_id if !dep.artifact_id.to_s.empty?
+      {:component => dep_name, :group_id => dep.group_id, :version => dep.version_requested, :license => name}
     end
 
 
