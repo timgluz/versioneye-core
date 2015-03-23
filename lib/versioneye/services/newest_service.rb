1a39763d33a64378120f34f623496fcbed57c1d0
reiz <robert.reiz.81@gmail.com>
2015-03-23 14:32:42 +0100

    Update NewestService!

--- lib/versioneye/services/newest_service.rb
+++ lib/versioneye/services/newest_service.rb
@@ -1,6 +1,52 @@
 class NewestService < Versioneye::Service
 
 
+  def self.create_newest( product, version_number, logger = nil )
+    newest = Newest.fetch_newest( product.language, product.prod_key, version_number )
+    return nil if newest
+
+    newest = Newest.new
+    newest.name       = product.name
+    newest.language   = product.language
+    newest.prod_key   = product.prod_key
+    newest.version    = version_number
+    newest.prod_type  = product.prod_type
+    newest.product_id = product._id.to_s
+    newest.save
+  rescue => e
+    log_exception e, logger
+    false
+  end
+
+  
+  def self.create_notifications(product, version_number, logger = nil)
+    new_notifications = 0
+    subscribers = product.users
+    return new_notifications if subscribers.nil? || subscribers.empty?
+
+    subscribers.each do |subscriber|
+      success = create_notification( subscriber, product, version_number, logger )
+      new_notifications += 1 if success
+    end
+    new_notifications
+  rescue => e
+    log_exception e, logger
+    false
+  end
+
+  def self.create_notification user, product, version_number, logger
+    notification            = Notification.new
+    notification.user       = user
+    notification.product    = product
+    notification.version_id = version_number
+    notification.save
+  rescue => e
+    log_exception e, logger
+    false
+  end
+
+
+
   def self.run_worker
     loop do 
       post_process
@@ -108,6 +154,19 @@ class NewestService < Versioneye::Service
     def self.multi_log msg 
       p msg 
       log.info msg 
+    end
+
+
+    def self.log_exception e, logger = nil
+      if logger
+        logger.error e.message
+        logger.error e.backtrace.join("\n")
+      else
+        p e.message
+        p e.backtrace.join("\n")
+        log.error e.message
+        log.error e.backtrace.join("\n")
+      end
     end
 
 
