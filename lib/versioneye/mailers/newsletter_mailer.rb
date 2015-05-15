1b97cdf22722f59e3547f234944610ad4746cf17
reiz <robert.reiz.81@gmail.com>
2015-05-15 10:39:09 +0200

    Add new newsletter.

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -8,7 +8,7 @@ class NewsletterMailer < ActionMailer::Base
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => 'Biicode Integration') do |format|
+    mail(:to => @user.email, :subject => 'PHP Security Vulnerabilities') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
