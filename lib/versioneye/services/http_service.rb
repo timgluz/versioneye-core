56806feab195fd95db721a41427061c6a581ff36
reiz <robert.reiz.81@gmail.com>
2015-06-01 20:59:44 +0200

    Remove unused code.

--- lib/versioneye/services/http_service.rb
+++ lib/versioneye/services/http_service.rb
@@ -5,14 +5,7 @@ class HttpService < Versioneye::Service
     http = Net::HTTP.new uri.host, uri.port
     http.read_timeout = timeout # in seconds 
     if uri.port == 443
-      curl_ca_bundle  = '/opt/local/share/curl/curl-ca-bundle.crt'
-      ca_certificates = '/usr/lib/ssl/certs/ca-certificates.crt'
       http.use_ssl = true
-      if File.exist?(curl_ca_bundle)
-        http.ca_file = curl_ca_bundle
-      elsif File.exist?(ca_certificates)
-        http.ca_file = ca_certificates
-      end
     end
     path  = uri.path
     query = uri.query
