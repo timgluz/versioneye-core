38d9a3d66c20154f15a3864880132e252376cb41
reiz <robert.reiz.81@gmail.com>
2015-06-04 16:52:14 +0200

    Refactor ProjectBatchUpdateService

--- lib/versioneye/services/project_batch_update_service.rb
+++ lib/versioneye/services/project_batch_update_service.rb
@@ -14,7 +14,6 @@ class ProjectBatchUpdateService < Versioneye::Service
   rescue => e
     log.error e.message
     log.error e.backtrace.join("\n")
-    nil
   end
 
 
@@ -35,15 +34,28 @@ class ProjectBatchUpdateService < Versioneye::Service
     uns = UserNotificationSetting.fetch_or_create_notification_setting( user )
     return nil if uns.project_emails == false
 
-    projects = fetch_projects user, period
+    perform_update user, period  
+
+    projects     = fetch_affected_projects user, period
+    col_projects = fetch_affected_collaboration_projects user, period
+    return nil if (projects.nil? || projects.empty?) && (col_projects.nil? || col_projects.empty?)
+
+    ProjectMailer.projectnotifications_email( user, projects, col_projects, period ).deliver
+  rescue => e 
+    log.error e.message
+    log.error e.backtrace.join("\n")
+  end
+
+
+  # Update the projects. Re parse the project files and update the numbers!
+  def self.perform_update user, period
+    projects     = fetch_projects user, period
     col_projects = fetch_collaboration_projects user, period
-    return nil if (projects.nil? || projects.empty?) && col_projects.empty?
+    return nil if (projects.nil? || projects.empty?) && (col_projects.nil? || col_projects.empty?)
 
     log.info "process #{period} projects for #{user.fullname}"
     update_projects projects, false
     update_projects col_projects, false 
-
-    ProjectMailer.projectnotifications_email( user, projects, col_projects, period ).deliver
   end
 
 
@@ -51,8 +63,7 @@ class ProjectBatchUpdateService < Versioneye::Service
     return nil if projects.nil? || projects.empty?
 
     projects.each do |project|
-      msg = "project_#{project.id.to_s}:::#{send_email}"
-      ProjectUpdateProducer.new( msg )
+      ProjectUpdateService.update project, send_email
     end
   end
 
@@ -66,17 +77,30 @@ class ProjectBatchUpdateService < Versioneye::Service
       user.projects.by_period( period ).parents
     end
 
+    def self.fetch_affected_projects user, period
+      return [] if user.projects.nil? || user.projects.empty?
+
+      user.projects.by_period( period ).parents.any_of({:out_number_sum.gt => 0},{:licenses_red_sum.gt => 0})
+    end
 
     def self.fetch_collaboration_projects user, period
       collaborations = ProjectCollaborator.by_user( user ).where(:period => period)
       projects = []
       collaborations.each do |collab|
-        if collab.project
-          projects << collab.project 
-        else
-          log.error "Collaborated project doesnt exists: `#{collab.to_json}`"
+        if collab.project.nil?
           collab.remove
+          next 
         end
+        projects << collab.project 
+      end
+      projects
+    end
+
+    def self.fetch_affected_collaboration_projects user, period
+      projects = []
+      pros = fetch_collaboration_projects user, period
+      pros.each do |project|
+        projects << project if project.licenses_red_sum > 0 || project.out_number_sum > 0 
       end
       projects
     end
---
+++ spec/versioneye/services/project_batch_update_service_spec.rb
@@ -0,0 +1,125 @@
+require 'spec_helper'
+
+describe ProjectBatchUpdateService do
+
+
+  describe 'update_all' do
+
+    it 'updates a project' do
+      product = ProductFactory.create_for_cocoapods 'JSONKit', '2.0.0'
+      expect( product.save ).to be_truthy
+      
+      user    = UserFactory.create_new 1
+      
+      parser  = PodfileParser.new
+      project = parser.parse_file './spec/fixtures/files/pod_file/example1/Podfile'
+      project.name = 'Podfile'
+      project.make_project_key!
+      project.user_id = user.ids 
+      project.period = Project::A_PERIOD_DAILY
+      project.sum_own!
+      expect( project.save ).to be_truthy
+      expect( project.out_number_sum ).to be > 0 
+
+      ActionMailer::Base.deliveries.clear
+      ProjectBatchUpdateService.update_all Project::A_PERIOD_DAILY
+      ActionMailer::Base.deliveries.size.should == 1
+    end
+
+    it 'doesnt send out email because project is up-to-date' do
+      user    = UserFactory.create_new 1
+      
+      parser  = PodfileParser.new
+      project = parser.parse_file './spec/fixtures/files/pod_file/example1/Podfile'
+      project.name = 'Podfile'
+      project.make_project_key!
+      project.user_id = user.ids 
+      project.period = Project::A_PERIOD_DAILY
+      project.sum_own!
+      expect( project.save ).to be_truthy
+      expect( project.out_number_sum ).to eq(0) 
+
+      ActionMailer::Base.deliveries.clear
+      ProjectBatchUpdateService.update_all Project::A_PERIOD_DAILY
+      expect( ActionMailer::Base.deliveries.size ).to eq(0)
+    end
+
+    it 'sends out email because child project is out-dated' do 
+      product = ProductFactory.create_for_gemfile('rails', '4.0.0')
+      product.save 
+
+      user    = UserFactory.create_new 1
+      
+      parser  = PodfileParser.new
+      project = parser.parse_file './spec/fixtures/files/pod_file/example1/Podfile'
+      project.name = 'Podfile'
+      project.make_project_key!
+      project.user_id = user.ids 
+      project.period = Project::A_PERIOD_DAILY
+      project.save 
+      
+      gemfile = "spec/fixtures/files/Gemfile"
+      file = File.open(gemfile, "rb")
+      content = file.read
+      parser = GemfileParser.new 
+      project_2 = parser.parse_content content
+      project_2.name = 'Gemfile'
+      project_2.make_project_key!
+      project_2.user_id = user.ids 
+      project_2.period = Project::A_PERIOD_DAILY
+      project_2.save 
+
+      project_2.parent_id = project.ids 
+      project_2.save 
+
+      ActionMailer::Base.deliveries.clear
+      ProjectBatchUpdateService.update_all Project::A_PERIOD_DAILY
+      expect( ActionMailer::Base.deliveries.size ).to eq(1)
+
+      project.reload 
+      expect( project.out_number_sum ).to eq(1) # It's 1 because child project has 1 out-dated dependency
+    end
+
+    it 'sends out 2 emails because of collaboration project' do 
+      product = ProductFactory.create_for_cocoapods 'JSONKit', '2.0.0'
+      expect( product.save ).to be_truthy
+
+      owner   = UserFactory.create_new 1023
+      user    = UserFactory.create_new 1024
+
+      parser  = PodfileParser.new
+      project = parser.parse_file './spec/fixtures/files/pod_file/example1/Podfile'
+      project.name = 'Podfile'
+      project.make_project_key!
+      project.user_id = owner.ids 
+      project.period = Project::A_PERIOD_DAILY
+      project.sum_own!
+      expect( project.save ).to be_truthy
+      expect( project.out_number_sum ).to eq(1) 
+      expect( project.licenses_red_sum ).to eq(0)
+
+      collaborator = ProjectCollaborator.new(:project_id => 'NaN',
+                                             :owner_id => owner._id,
+                                             :caller_id => owner._id, 
+                                             :user_id => user._id, 
+                                             :period => Project::A_PERIOD_DAILY )
+      expect( collaborator.save ).to be_truthy
+
+      collaborator = ProjectCollaborator.new(:project_id => project._id,
+                                             :owner_id => owner._id,
+                                             :caller_id => owner._id, 
+                                             :user_id => user._id, 
+                                             :period => Project::A_PERIOD_DAILY )
+      expect( collaborator.save ).to be_truthy
+      project.collaborators << collaborator
+      expect( project.save ).to be_truthy
+
+      ActionMailer::Base.deliveries.clear
+      ProjectBatchUpdateService.update_all Project::A_PERIOD_DAILY
+      expect( ActionMailer::Base.deliveries.size ).to eq(2)
+      expect( ProjectCollaborator.count ).to eq(1)
+    end
+
+  end
+
+end
