504a031331f3d07afecbd0ab2002c1361d80b074
reiz <robert.reiz.81@gmail.com>
2015-08-27 11:08:58 +0200

    Trim.

--- lib/versioneye/updaters/upload_updater.rb
+++ lib/versioneye/updaters/upload_updater.rb
@@ -2,13 +2,13 @@ class UploadUpdater < CommonUpdater
 
   def update( project, send_email = false )
     return nil if project.nil?
-    
-    out_number = 0 
-    dep_number = 0 
+
+    out_number = 0
+    dep_number = 0
     project.dependencies.each do |dep|
       outdated = ProjectdependencyService.outdated?( dep )
       out_number += 1 if outdated
-      dep_number += 1 
+      dep_number += 1
     end
     project.reload
 
@@ -17,16 +17,16 @@ class UploadUpdater < CommonUpdater
     SyncService.sync_project_async project # For Enterprise environment
 
     ProjectdependencyService.update_licenses_security project
-    
+
     unknown_licenses = ProjectService.unknown_licenses( project )
     red_licenses     = ProjectService.red_licenses( project )
     project.licenses_red = red_licenses.count
     project.licenses_unknown = unknown_licenses.count
     project.dep_number = dep_number
     project.out_number = out_number
-    project.sum_own! 
+    project.sum_own!
 
-    return project if send_email == false 
+    return project if send_email == false
     return project if project.user.email_inactive == false
 
     outdated_deps  = project.out_number > 0
