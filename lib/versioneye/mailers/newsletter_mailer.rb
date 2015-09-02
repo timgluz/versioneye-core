7be17bfcb9d4f8d36c539fc92a988f417fceb134
reiz <robert.reiz.81@gmail.com>
2015-09-02 13:47:25 +0200

    Updating newsletter.

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -1,10 +1,10 @@
 class NewsletterMailer < SuperMailer
 
-  
+
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => 'PHP Security Vulnerabilities') do |format|
+    mail(:to => @user.email, :subject => 'VersionEye SBT Plugin + Component Whitelist') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
