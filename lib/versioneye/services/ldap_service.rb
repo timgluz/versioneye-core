051d23e3ec82def7c374ca56c9a1dcbce1760254
reiz <robert.reiz.81@gmail.com>
2015-09-07 14:59:32 +0200

    Refactor LDAP Service

--- lib/versioneye/services/ldap_service.rb
+++ lib/versioneye/services/ldap_service.rb
@@ -6,31 +6,35 @@ class LdapService < Versioneye::Service
 
   def self.auth_by login, password, ldap = Net::LDAP.new
     Settings.instance.reload_from_db GlobalSetting.new
+
+    auth_method = Settings.instance.ldap_auth.to_s
+    auth_method = "simple" if auth_method.to_s.empty?
+
+    username = Settings.instance.ldap_username_pattern
+    username = username.gsub("LOGIN", login)
+
     filter = Settings.instance.ldap_filter
     filter = filter.gsub("LOGIN", login)
-    ldap = Net::LDAP.new() if ldap.nil?
-    ldap.host = Settings.instance.ldap_host
-    ldap.port = Settings.instance.ldap_port
-    ldap.bind_as(:base => Settings.instance.ldap_base,
-               :filter => filter,
-               :password => password )
-  rescue => e
-    log.error e.message
-    e.message
-  end
 
+    ldap_args = {:host => Settings.instance.ldap_host,
+                 :port => Settings.instance.ldap_port,
+                 :base => Settings.instance.ldap_base,
+                 :auth => {:username => username,
+                           :password => password,
+                           :method => auth_method.to_sym } }
 
-  def self.search_code login, ldap = Net::LDAP.new
-    filter = Settings.instance.ldap_filter
-    filter = filter.gsub("LOGIN", login)
-    ldap = Net::LDAP.new() if ldap.nil?
-    ldap.host = Settings.instance.ldap_host
-    ldap.port = Settings.instance.ldap_port
-    ldap.search(:base => Settings.instance.ldap_base,
-              :filter => filter)
+    ldap = Net::LDAP.new( ldap_args )
+
+    encryption = Settings.instance.ldap_encryption.to_s
+    if !encryption.empty? && !encryption.strip.downcase.eql?('none')
+      ldap.encryption( :method => encryption.to_sym )
+    end
+
+    if ldap.bind == true
+      return ldap.search(:base => Settings.instance.ldap_base, :filter => filter)
+    end
+
     result = ldap.get_operation_result
-    log.info "LDAP Code: #{result.code}. Message: #{result.message}. Error Message: #{result.error_message}"
-    return "" if result.message.eql?('Success') && result.error_message.to_s.empty?
     "Code: #{result.code}. Message: #{result.message} #{result.error_message}"
   rescue => e
     log.error e.message
