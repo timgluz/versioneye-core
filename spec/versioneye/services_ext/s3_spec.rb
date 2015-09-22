1da32e65301bfedfa96431b1ea07ec4ef563a541
reiz <robert.reiz.81@gmail.com>
2015-09-22 13:53:06 +0200

    Trim

--- spec/versioneye/services_ext/s3_spec.rb
+++ spec/versioneye/services_ext/s3_spec.rb
@@ -4,16 +4,16 @@ require 'spec_helper'
 describe S3 do
 
   describe 'infographic_url_for' do
-    it 'returns the url' do 
+    it 'returns the url' do
       region = 'eu-west-1'
-      
+
       Aws.config[:credentials] = creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 
       filename = 'actionmailer:3~2~7:runtime.png'
       url = S3.infographic_url_for filename
       url.to_s.should_not be_empty
       url.to_s.should eq("https://s3-eu-west-1.amazonaws.com/veye_test_infographics/actionmailer%3A3~2~7%3Aruntime.png")
-    end  
+    end
   end
 
   describe 'upload_fileupload - url_for - delete' do
@@ -21,20 +21,20 @@ describe S3 do
       region = 'eu-west-1'
 
       creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-      Aws.config[:credentials] = creds 
-      
+      Aws.config[:credentials] = creds
+
       gemfile = "spec/fixtures/files/Gemfile"
       file_attachment = Rack::Test::UploadedFile.new(gemfile, "application/octet-stream")
       file = {'datafile' => file_attachment}
 
       filename = S3.upload_fileupload file
       filename.should_not be_nil
-      filename.match(/_Gemfile\z/).should_not be_nil 
-      
+      filename.match(/_Gemfile\z/).should_not be_nil
+
       s3 = Aws::S3::Resource.new(:region => 'eu-west-1')
       bucket = s3.bucket( Settings.instance.s3_projects_bucket )
-      found = false 
-      bucket.objects.each do |obj| 
+      found = false
+      bucket.objects.each do |obj|
         found = true if obj.key.eql?(filename)
       end
       found.should be_truthy
@@ -47,8 +47,8 @@ describe S3 do
       resp = S3.delete filename
       s3 = Aws::S3::Resource.new(:region => 'eu-west-1')
       bucket = s3.bucket( Settings.instance.s3_projects_bucket )
-      found = false 
-      bucket.objects.each do |obj| 
+      found = false
+      bucket.objects.each do |obj|
         found = true if obj.key.eql?(filename)
       end
       found.should be_falsey
