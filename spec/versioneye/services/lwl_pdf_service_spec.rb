9dd2f9497df681297edde8c7ec67f495f2b9d423
reiz <robert.reiz.81@gmail.com>
2015-11-05 20:25:18 +0100

    Add more tests

--- spec/versioneye/services/lwl_pdf_service_spec.rb
+++ spec/versioneye/services/lwl_pdf_service_spec.rb
@@ -145,6 +145,33 @@ describe LwlPdfService do
       expect(project_1.lwl_pdf_list[:violated].count).to eq(0)
     end
 
+    it 'fils the dto with 2 lines from 1 dependency, because of dual license' do
+      user = UserFactory.create_new 1
+      lwl  = LicenseWhitelist.new({:name => 'OkForMe', :user_id => user.id})
+      expect(lwl.save).to be_truthy
+      LicenseWhitelistService.add user, 'OkForMe', 'MIT'
+
+      prod_1    = ProductFactory.create_new 1
+      project_1 = ProjectFactory.create_new user
+      project_1.license_whitelist_id = lwl.id
+      project_1.save
+
+      dep_1 = ProjectdependencyFactory.create_new project_1, prod_1, true
+      dep_1.license_caches = []
+      dep_1.license_caches << LicenseCach.new({:name => 'MIT', :on_whitelist => true})
+      dep_1.license_caches << LicenseCach.new({:name => 'GPL-3.0', :on_whitelist => false})
+      dep_1.save
+
+      expect(project_1.lwl_pdf_list).to be_nil
+      described_class.fill_dto project_1, true
+
+      expect(project_1.lwl_pdf_list[:unknown].count).to eq(0)
+      expect(project_1.lwl_pdf_list[:whitelisted].count).to eq(1)
+      expect(project_1.lwl_pdf_list[:violated].count).to eq(1)
+      expect(project_1.lwl_pdf_list[:violated].first[:license]).to eq('GPL-3.0')
+      expect(project_1.lwl_pdf_list[:whitelisted].first[:license]).to eq('MIT')
+    end
+
   end
 
 end
