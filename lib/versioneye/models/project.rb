1f29c020dd2df24573c1c49a88aab2aab4bfe093
reiz <robert.reiz.81@gmail.com>
2015-11-18 23:42:49 +0100

    Handle empty dependencies.

--- lib/versioneye/models/project.rb
+++ lib/versioneye/models/project.rb
@@ -325,6 +325,7 @@ class Project < Versioneye::Model
   end
 
   def overwrite_dependencies( new_dependencies )
+    return nil if new_dependencies.nil? || new_dependencies.empty?
     muted_deps    = muted_prod_keys
     muted_keys    = muted_deps[:keys]
     mute_messages = muted_deps[:messages]
