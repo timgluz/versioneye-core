56d0097e16482b14fcb254b6fd0732bd06c1991b
reiz <robert.reiz.81@gmail.com>
2015-11-19 18:44:12 +0100

    Update Lwl Service to export x projects.

--- lib/versioneye/services/lwl_csv_service.rb
+++ lib/versioneye/services/lwl_csv_service.rb
@@ -1,14 +1,31 @@
 class LwlCsvService < LwlService
 
 
+  def self.process_all projects, lwl, cwl, flatten = true, write_to_disk = false
+    csv_string = CSV.generate do |csv|
+      projects.each do |project|
+        fill_dto project, flatten
+        csv << ['', '', '', '']
+        csv << ["Project:", "#{project.name} (#{project.ids})", '', '']
+        csv << ["Status", "Component", "Version", "License"]
+        fill_components project, csv
+      end
+      fill_license_whitelist lwl, csv
+      fill_component_whitelist cwl, csv
+    end
+    csv_string
+  end
+
+
   def self.process project, exclude_kids = false, flatten = true, write_to_disk = false
     fill_dto project, flatten
-
     csv_string = CSV.generate do |csv|
+      csv << ['', '', '', '']
+      csv << ["Project:", "#{project.name} (#{project.ids})", '', '']
       csv << ["Status", "Component", "Version", "License"]
       fill_components project, csv
-      fill_license_whitelist project, csv
-      fill_component_whitelist project, csv
+      fill_license_whitelist project.license_whitelist, csv
+      fill_component_whitelist project.component_whitelist, csv
     end
     csv_string
   end
@@ -28,25 +45,25 @@ class LwlCsvService < LwlService
     end
 
 
-    def self.fill_license_whitelist project, csv
-      return nil if project.license_whitelist.nil? || project.license_whitelist.license_elements.empty?
+    def self.fill_license_whitelist lwl, csv
+      return nil if lwl.nil? || lwl.license_elements.empty?
 
       csv << ['', '', '', '']
-      csv << ['License Whitelist', "#{project.license_whitelist.name} (#{project.license_whitelist.id})", '', '']
+      csv << ['License Whitelist', "#{lwl.name} (#{lwl.ids})", '', '']
       csv << ['Bill of materials:', '', '', '']
-      project.license_whitelist.license_elements.each do |lwle|
+      lwl.license_elements.each do |lwle|
         csv << ['', lwle.name, '', '']
       end
     end
 
 
-    def self.fill_component_whitelist project, csv
-      return nil if project.component_whitelist.nil? || project.component_whitelist.components.empty?
+    def self.fill_component_whitelist cwl, csv
+      return nil if cwl.nil? || cwl.components.empty?
 
       csv << ['', '', '', '']
-      csv << ['Component Whitelist', "#{project.component_whitelist.name} (#{project.component_whitelist.id})", '', '']
+      csv << ['Component Whitelist', "#{cwl.name} (#{cwl.ids})", '', '']
       csv << ['Bill of materials:', '', '', '']
-      project.component_whitelist.components.each do |component|
+      cwl.components.each do |component|
         csv << ['', component.to_s, '', '']
       end
     end
