39e15c1fbcdd501222bde162eead45e730dfa416
reiz <robert.reiz.81@gmail.com>
2015-11-21 12:54:19 +0100

    Add more attributes to Organisation.
    Ensure that Orga name is unique.

--- lib/versioneye/models/organisation.rb
+++ lib/versioneye/models/organisation.rb
@@ -4,10 +4,26 @@ class Organisation < Versioneye::Model
   include Mongoid::Timestamps
 
   field :name, type: String
+  field :company, type: String
+  field :location, type: String
+  field :email, type: String
+  field :website, type: String
 
   has_many :projects
   has_many :teams
   # has_many license_whitelists
   # has_many component_whitelists
+
+  validates_presence_of :name, :message => 'is mandatory!'
+  validates_uniqueness_of :name, :message => 'exist already.'
+  index({ username: 1 }, { name: "username_index", background: true, unique: true })
+
+  def to_s
+    name
+  end
+
+  def to_param
+    name
+  end
 
 end
--- lib/versioneye/services/organisation_service.rb
+++ lib/versioneye/services/organisation_service.rb
@@ -1,7 +1,11 @@
 class OrganisationService < Versioneye::Service
 
+
   def self.create_new user, name
-    orga = Organisation.new({:name => name})
+    if !Organisation.where({:name => name.downcase}).empty?
+      raise "Organisation with name '#{name}' exists already. Please choose another name."
+    end
+    orga = Organisation.new({:name => name.downcase})
     orga.save
     team = Team.new(:name => Team::A_OWNERS)
     team.add_member user
@@ -9,5 +13,23 @@ class OrganisationService < Versioneye::Service
     team.save
     orga
   end
+
+
+  def self.index user
+    tms = TeamMember.where(:user_id => user.ids)
+    return [] if tms.empty?
+
+    organisations = []
+    orga_ids = []
+    tms.each do |tm|
+      orga = tm.team.organisation
+      next if orga_ids.include?(orga.ids)
+
+      orga_ids.push(orga.ids)
+      organisations.push(orga)
+    end
+    organisations
+  end
+
 
 end
