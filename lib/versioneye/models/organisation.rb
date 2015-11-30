559f479531a8c98f2a0a34f9d2121d80024a31a9
reiz <robert.reiz.81@gmail.com>
2015-11-30 20:06:00 +0100

    Set name index for organisation

--- lib/versioneye/models/organisation.rb
+++ lib/versioneye/models/organisation.rb
@@ -11,12 +11,13 @@ class Organisation < Versioneye::Model
 
   has_many :projects
   has_many :teams
-  # has_many license_whitelists
-  # has_many component_whitelists
+  has_many :license_whitelists
+  has_many :component_whitelists
 
   validates_presence_of :name, :message => 'is mandatory!'
   validates_uniqueness_of :name, :message => 'exist already.'
-  index({ username: 1 }, { name: "username_index", background: true, unique: true })
+
+  index({ name: 1 }, { name: "name_index", background: true, unique: true })
 
   def to_s
     name
