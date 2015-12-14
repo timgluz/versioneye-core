38110219acff1418d1316d3c51fe70ef2de3d103
reiz <robert.reiz.81@gmail.com>
2015-12-14 11:52:50 +0100

    Improve finding deps with unknown licenses.

--- lib/versioneye/models/project.rb
+++ lib/versioneye/models/project.rb
@@ -235,7 +235,7 @@ class Project < Versioneye::Model
   def unknown_license_deps
     deps = []
     projectdependencies.each do |dep|
-      if (dep.license_caches.to_a.empty?) || (dep.license_caches.count == 1 && dep.license_caches.first.name.casecmp('unknown') == 0)
+      if (dep.license_caches.nil? || dep.license_caches.to_a.empty?) || (dep.license_caches.count == 1 && dep.license_caches.first.name.casecmp('unknown') == 0)
         deps.push(dep)
       end
     end
