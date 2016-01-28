c1d5eae792c421b9271a6f22b433ace73eb63b57
reiz <robert.reiz.81@gmail.com>
2016-01-28 13:26:48 +0100

    Add Proxy Settings to HTTP connections.

--- lib/versioneye/services/http_service.rb
+++ lib/versioneye/services/http_service.rb
@@ -1,8 +1,24 @@
 class HttpService < Versioneye::Service
 
   def self.fetch_response url, timeout = 60
+
+    env = Settings.instance.environment
+    proxy_addr = GlobalSetting.get env, 'proxy_addr'
+    proxy_port = GlobalSetting.get env, 'proxy_port'
+    proxy_user = GlobalSetting.get env, 'proxy_user'
+    proxy_pass = GlobalSetting.get env, 'proxy_pass'
+
     uri  = URI.parse url
-    http = Net::HTTP.new uri.host, uri.port
+    http = nil
+
+    if proxy_addr.to_s.empty?
+      http = Net::HTTP.new uri.host, uri.port
+    elsif !proxy_addr.to_s.empty? && !proxy_user.to_s.empty? && !proxy_pass.to_s.empty?
+      http = Net::HTTP.new uri.host, uri.port, proxy_addr, proxy_port, proxy_user, proxy_pass
+    elsif !proxy_addr.to_s.empty? && proxy_user.to_s.empty?
+      http = Net::HTTP.new uri.host, uri.port, proxy_addr, proxy_port
+    end
+
     http.read_timeout = timeout # in seconds
     if uri.port == 443
       http.use_ssl = true
