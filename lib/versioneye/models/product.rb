f1717484ad05945c69b8762d2b7c8db40e485827
reiz <robert.reiz.81@gmail.com>
2016-02-02 15:09:26 +0100

    Add index and bugfix to product.

--- lib/versioneye/models/product.rb
+++ lib/versioneye/models/product.rb
@@ -48,7 +48,8 @@ class Product < Versioneye::Model
   validates_presence_of :name     , :message => 'name      is mandatory!'
 
   # For indexing use task: rake db:mongoid:create_indexes
-  index({ prod_key: 1, language: 1 },    { name: "prod_key_language_index", unique: true, background: true })
+  index({ prod_key:    1, language: 1 },    { name: "prod_key_language_index"   , unique: true, background: true })
+  index({ prod_key_dc: 1, language: 1 },    { name: "prod_key_dc_language_index", unique: true, background: true })
   index({ group_id: 1, artifact_id: 1 }, { name: "group_id_artifact_id_index", background: true })
   index({ name: 1 },                     { name: "name_index", background: true })
   index({ name_downcase: 1 },            { name: "name_downcase_index", background: true })
@@ -125,7 +126,7 @@ class Product < Versioneye::Model
 
     product = Product.find_by_lang_key( lang, key )
     product = Product.find_by_lang_key( lang, key.downcase ) if product.nil?
-    product = Product.where(:language => lang, :prod_key_dc => key.downcase) if ( product.nil? && lang.eql?( A_LANGUAGE_NODEJS ) )
+    product = Product.where(:language => lang, :prod_key_dc => key.downcase).first if ( product.nil? && lang.eql?( A_LANGUAGE_NODEJS ) )
     product
   end
 
