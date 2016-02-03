6699eae2de17515c787bb6e828bbb014cbf719fa
reiz <robert.reiz.81@gmail.com>
2016-02-03 12:28:40 +0100

    Use static methods.

--- lib/versioneye/updaters/github_updater.rb
+++ lib/versioneye/updaters/github_updater.rb
@@ -38,8 +38,8 @@ class GithubUpdater < CommonUpdater
   def parse project_file, token = nil
     content   = GitHubService.pure_text_from project_file
     file_name = GitHubService.filename_from project_file
-    parser    = parser_for file_name
-    parse_content parser, content, file_name, token
+    parser    = ProjectParseService.parser_for file_name
+    ProjectParseService.parse_content parser, content, file_name, token
   end
 
 
