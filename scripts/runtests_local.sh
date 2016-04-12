c90b3535bcb43509938463e8e7d93d55eb528359
reiz <robert.reiz.81@gmail.com>
2016-04-12 09:50:49 +0300

    Rename NotificationWatcher to Watcher.

--- scripts/runtests_local.sh
+++ scripts/runtests_local.sh
@@ -16,7 +16,7 @@ echo "Going to run all specs"
 export RAILS_ENV="test"
 echo "Rails mode: $RAILS_ENV"
 
-rspec
+rspec spec/versioneye/models/watcher_spec.rb
 
 export RAILS_ENV="development"
 echo "Rails mode: $RAILS_ENV"
