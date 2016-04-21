b56b119dcea6e768b2512be079f8b4f8b8d76a65
reiz <robert.reiz.81@gmail.com>
2016-04-21 19:18:29 +0300

    Add enabled to the ArtifactoryConnection.

--- lib/versioneye/models/artifactory_connection.rb
+++ lib/versioneye/models/artifactory_connection.rb
@@ -3,11 +3,12 @@ class ArtifactoryConnection < Versioneye::Model
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :art_user , type: String
-  field :art_pass , type: String
-  field :art_id   , type: String
-  field :art_url  , type: String
-  field :art_token, type: String
+  field :art_user   , type: String
+  field :art_pass   , type: String
+  field :art_id     , type: String
+  field :art_url    , type: String
+  field :art_token  , type: String
+  field :art_enabled, type: Boolean
 
   validates :art_user , presence: true
   validates :art_pass , presence: true
