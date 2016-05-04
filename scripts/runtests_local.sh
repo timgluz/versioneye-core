4abe499793b4a0e0a4c4a635e08fa86d895d6f12
reiz <robert.reiz.81@gmail.com>
2016-05-04 23:00:35 +0300

    Update locale test runner

--- scripts/runtests_local.sh
+++ scripts/runtests_local.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# MongoDB
 export DB_PORT_27017_TCP_ADDR=172.16.148.134
 export DB_PORT_27017_TCP_PORT=27017
 
+# ElasticSearch
 export ES_PORT_9200_TCP_ADDR=172.16.148.134
 export ES_PORT_9200_TCP_PORT=9200
 
+# RabbitMQ
 export RM_PORT_5672_TCP_ADDR=172.16.148.134
 export RM_PORT_5672_TCP_PORT=5672
 
+# Memcache
 export MC_PORT_11211_TCP_ADDR=172.16.148.134
 export MC_PORT_11211_TCP_PORT=11211
 
@@ -17,8 +21,6 @@ export RAILS_ENV="test"
 echo "Rails mode: $RAILS_ENV"
 
 rspec
-# rspec # spec/versioneye/mailers/xray_mailer_spec.rb
-# rspec spec/versioneye/services/watcher_service_spec.rb spec/versioneye/mailers/xray_mailer_spec.rb
 
 export RAILS_ENV="development"
 echo "Rails mode: $RAILS_ENV"
