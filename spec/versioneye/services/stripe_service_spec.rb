662b59affafe2413e1bcf57efe9caed378ae9491
reiz <robert.reiz.81@gmail.com>
2016-05-09 10:06:26 +0200

    Remove test for legacy stripe account.

--- spec/versioneye/services/stripe_service_spec.rb
+++ spec/versioneye/services/stripe_service_spec.rb
@@ -49,12 +49,6 @@ describe StripeService do
       cust.should be_nil
     end
 
-    it 'fetches an legacy customer' do
-      customer_id = "cus_46xjeG6vago0z0"
-      cust = described_class.fetch_customer customer_id, Settings.instance.stripe_legacy_secret_key
-      cust.should_not be_nil
-    end
-
   end
 
   describe 'update_customer' do
