61c00332516881a1d5d4e085f577f6dfa02ae17c
reiz <robert.reiz.81@gmail.com>
2016-05-17 17:17:49 +0200

    Refactor CSV team and user importer for SAP.

--- lib/versioneye/importers/sap_team_user_importer.rb
+++ lib/versioneye/importers/sap_team_user_importer.rb
@@ -1,33 +1,36 @@
 class SapTeamUserImporter
 
+
   def self.process filepath = "/Users/reiz/SAP-ECP-DLs.csv", orga_name = "ecp"
     orga = Organisation.where(:name => orga_name).first
     CSV.foreach(filepath) do |row|
-      team_name  = row[0].gsub(" ", "_").downcase
-      last_name  = row[1]
-      first_name = row[2]
-      email      = row[3]
+      team_name  = row[0].gsub(" ", "-").downcase
+      email      = row[1]
+      next if email.match(/@/).nil?
 
-      user = create_user last_name, first_name, email
+      name = email.split("@").first
+
+      user = create_user name, email
       team = create_team orga, team_name
       team.add_member user
     end
   end
 
 
-  def self.create_user last_name, first_name, email
+  def self.create_user name, email
     user = User.find_by_email email
     return user if user
 
     user = User.new
-    user.username = "#{first_name}_#{last_name}".downcase
+    user.username = name.downcase.gsub(".", "_")
     user.cleanup_username
     user.ensure_unique_username
-    user.fullname = "#{first_name}_#{last_name}"
+    user.fullname = name.gsub(".", " ")
     user.email = email
     user.terms = true
     user.datenerhebung = true
     user.save
+    user.update_password user.username
     user
   end
 
