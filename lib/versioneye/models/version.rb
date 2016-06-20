d726390b5baa984db985df451be9a226feb667d4
reiz <robert.reiz.81@gmail.com>
2016-06-20 09:10:07 +0200

    Add prefer_global to version

--- lib/versioneye/models/version.rb
+++ lib/versioneye/models/version.rb
@@ -16,6 +16,7 @@ class Version < Versioneye::Model
   field :sha1           , type: String
   field :sha256         , type: String
   field :md5            , type: String
+  field :prefer_global  , type: Boolean, default: false
 
   embedded_in :product
 
