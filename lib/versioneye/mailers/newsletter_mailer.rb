e78a30ded33e3c663d9650f7dec8b841ec044085
reiz <robert.reiz.81@gmail.com>
2016-06-28 15:00:52 +0200

    Update the newsletter.

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => 'PHP Runtime Compatability, Security PDF & CloudRail API') do |format|
+    mail(:to => @user.email, :subject => 'VersionEye goes open source!') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
