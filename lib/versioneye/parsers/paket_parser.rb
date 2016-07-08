9b6b8f79007b0eef8c5bcf935634a7f4f50995eb
Timo Sulg <timgluz@gmail.com>
2016-07-08 18:25:35 +0200

    extract all the dependencies from the file;

--- lib/versioneye/parsers/paket_parser.rb
+++ lib/versioneye/parsers/paket_parser.rb
@@ -1,34 +1,20 @@
 require 'versioneye/parsers/common_parser'
+require 'semverly'
 
 class PaketParser < CommonParser
-  attr_reader :rules, :comperators
+  attr_reader :comperators
 
   def initialize
-    #RULES for SEMVER
-    numeric = '\\d+'
-    ident = "[\\w-]" #identificator aka textual value
-    prerelease_info = "\\-(?<prerelease>#{ident}[\\.#{ident}]*)" #matches release info: -alpha.1
-    build_info = "\\+(?<build>#{ident}[\\.#{ident}]*)" #matches build info
-    version = "(?<version>(#{numeric})(\\.(#{numeric})(\\.(#{numeric}))?)?)"
-    semver = "#{version}(#{prerelease_info})?(#{build_info})?"
-    empty_string  = "^\\s*$"  # "" "   "
-
-    #TODO: checkout SemVer module included by GEMFILE
-    @rules = {
-      version: Regexp.new(version, Regexp::EXTENDED),
-      semver: Regexp.new(semver, Regexp::EXTENDED),
-      empty: Regexp.new(empty_string, Regexp::EXTENDED)
-    }
-
-    @comperators = Set.new ['~>', '==', '<=', '>=', '=', '<', '>']
+    @comperators = Set.new(['~>', '==', '<=', '>=', '=', '<', '>'])
   end
 
   def semver?(version_label)
     return false if version_label.nil?
+    res = SemVer.parse(version_label)
 
-    return true if rules[:semver].match(version_label)
-    return true if rules[:version].match(version_label)
-    return true if rules[:empty].match(version_label)
+    return (res ? true : false)
+  rescue
+    log.error "semver?: failed to parse version #{version_label}"
     return false
   end
 
@@ -37,11 +23,34 @@ class PaketParser < CommonParser
     comperators.include?(comperator)
   end
 
+  def parse(url)
+    if url.to_s.empty?
+      logger.error "PaketParser: url was empty"
+      return nil
+    end
 
-  def parse_doc(paket_doc)
-    lines = paket_doc.split(/[\n\r]/)
+    body = self.fetch.response_body(url)
+    project = init_project(url)
+    deps = parse_content(body)
+    save_project_dependencies(project, deps)
+
+    project
+  end
+
+  def init_project(url)
+    Project.new({
+      project_type: Project::A_TYPE_NUGET,
+      language: Project::A_LANGUAGE_CSHARP,
+      name: "Paket Project",
+      url: url
+    })
+  end
+
+  def parse_content(paket_doc)
+    lines = paket_doc.split(/\n+/)
     if lines.empty?
       log.error "PaketParser: got empty file"
+      return
     end
 
     deps = []
@@ -49,23 +58,23 @@ class PaketParser < CommonParser
     lines.each do |line| 
       new_group, parsed_dep = parse_line(current_group, line)
       current_group = new_group if current_group != new_group
-      
-      next if parsed_dep.nil?
-
-      deps << parsed_dep
+    
+      deps << parsed_dep if parsed_dep
     end
+
+    deps
   end
 
+  #extracts package-id, version and group/profile from the file
   def parse_line(current_group, line)
     tkns =  line.to_s.split( /\s+/ )
     return [current_group, group_doc] if tkns.empty?
 
-    
     if tkns.count == 1 and tkns.first == '' and current_group != '*'
       return ['*', nil] #switch back to default group
     end
 
-    tkns.shift if tkns.first = '' #remove empty string that caused by tabulation in groups
+    tkns.shift if tkns.first == '' #remove empty string that caused by tabulation in groups
     
     source_id, pkg_id, comperator, version = tkns
     source_id.downcase!
@@ -97,7 +106,7 @@ class PaketParser < CommonParser
       if comperator?(comperator) and version
         dep_dt[:comperator] = comperator
         dep_dt[:version] = version
-      elsif comperator.to_s.count > 0
+      elsif comperator.to_s.size > 0
         dep_dt[:comperator] = '='
         dep_dt[:version] = comperator
       end
@@ -119,6 +128,56 @@ class PaketParser < CommonParser
     else
       return [current_group, nil]
     end
-
   end
+
+  def save_project_dependencies(project, deps)
+    deps.to_a.each {|dep| save_dependency(project, dep) }
+    project
+  end
+
+  #TODO: finish
+  def save_dependency(project, dep)
+    product = nil
+
+    version_label = dep[:version]
+    if version_label.to_s.empty?
+      dep[:version] = product.version
+    end
+
+    dep_db = init_dependency(dep)
+
+    #TODO: finish
+    #parse_requested_version(version_label, dep_db, product)
+    
+    project.out_number += 1 if dep_db.outdated?
+    project.unknown_number += 1 if product.nil?
+    project.projectdependencies.push dep_db
+
+    project
+  end
+
+  def init_dependency(dep)
+    dep_scope = case dep[:group]
+                when '*'
+                  Dependency::A_SCOPE_COMPILE
+                when /test/i
+                  Dependency::A_SCOPE_TEST
+                when /development/i
+                  Dependency::A_SCOPE_DEVELOPMENT
+                when /dev/i
+                  Dependency::A_SCOPE_DEVELOPMENT
+                else
+                  dep[:group]
+                end
+
+    dep_db = ProjectDependency.new(
+      language: Product::A_LANGUAGE_CSHARP,
+      name: dep[:prod_key],
+      prod_key: dep[:prod_key],
+      version_label: dep[:version],
+      target: nil, #TODO: extract it from file
+      scope: dep_scope
+    )
+  end
+
 end
--- spec/versioneye/parsers/paket_parser_spec.rb
+++ spec/versioneye/parsers/paket_parser_spec.rb
@@ -4,6 +4,9 @@ describe PaketParser do
   let(:test_file_url){
     "spec/fixtures/files/nuget/paket.dependencies"
   }
+  let(:test_file){
+    File.read("spec/fixtures/files/nuget/paket.dependencies")
+  }
 
   let(:prod1){
     FactoryGirl.create(
@@ -16,28 +19,111 @@ describe PaketParser do
     )
   }
 
-  let(:parser){NugetParser.new}
+  let(:parser){ PaketParser.new }
 
-  context "parser rules" do
-    it "matches version numbers" do
-      version = parser.rules[:version]
-      "1".match(version).should_not be_nil
-      "1.0".match(version).should_not be_nil
-      "1.1.0".match(version).should_not be_nil
-      "  1.1.0".match(version).should_not be_nil
-      "1.1.0  ".match(version).should_not be_nil
-      "  1.1.0  ".match(version).should_not be_nil
+  context "comperator?" do
+    it "matches with all the supported comperators" do
+      expect(parser.comperator?('~>')).to be_truthy
+      expect(parser.comperator?('==')).to be_truthy
+      expect(parser.comperator?('<=')).to be_truthy
+      expect(parser.comperator?('>=')).to be_truthy
+      expect(parser.comperator?('=')).to be_truthy
+      expect(parser.comperator?('<')).to be_truthy
+      expect(parser.comperator?('<')).to be_truthy 
     end
+  end
 
-    it "matches semantic versions" do
-      semver = parser.rules[:semver]
-      "1.0-alpha".match(semver).should_not be_nil
-      "1.0-alpha.1".match(semver).should_not be_nil
-      "1.0-alpha-1.0".match(semver).should_not be_nil
-      "1.0+build".match(semver).should_not be_nil
-      "1.0+build.1".match(semver).should_not be_nil
-      "1.0-alpha+build".match(semver).should_not be_nil
-      "1.0-alpha.1.2+build.2".match(semver).should_not be_nil
+  context "semver?" do
+    it "returns true for all the proper semvers" do
+      expect(parser.semver?('2.6.4')).to be_truthy
+      expect(parser.semver?('1.0+build54')).to be_truthy
+    end
+  end
+
+  context "parser_doc" do
+    it "extracts correct dependencies from the file" do
+    
+    deps = parser.parse_doc(test_file)
+    expect(deps.count).to eq(13)
+
+    expect(deps[0][:group]).to eq('*')
+    expect(deps[0][:source]).to eq('nuget')
+    expect(deps[0][:prod_key]).to eq('NUnit')
+    expect(deps[0][:comperator]).to eq('~>')
+    expect(deps[0][:version]).to eq('2.6.3')
+
+    expect(deps[1][:group]).to eq('*')
+    expect(deps[1][:source]).to eq('nuget')
+    expect(deps[1][:prod_key]).to eq('DotNetZip')
+    expect(deps[1][:comperator]).to eq('>=')
+    expect(deps[1][:version]).to eq('1.9')
+
+    expect(deps[2][:group]).to eq('*')
+    expect(deps[2][:source]).to eq('nuget')
+    expect(deps[2][:prod_key]).to eq('SourceLink.Fake')
+    expect(deps[2][:comperator]).to eq('*')
+    expect(deps[2][:version]).to eq('')
+
+    expect(deps[3][:group]).to eq('*')
+    expect(deps[3][:source]).to eq('github')
+    expect(deps[3][:prod_key]).to eq('forki/Fs')
+    expect(deps[3][:comperator]).to eq('*')
+    expect(deps[3][:version]).to eq('')
+
+    expect(deps[4][:group]).to eq('*')
+    expect(deps[4][:source]).to eq('github')
+    expect(deps[4][:prod_key]).to eq('fsharp/fsfoundation')
+    expect(deps[4][:comperator]).to eq('=')
+    expect(deps[4][:version]).to eq('gh-pages')
+
+    expect(deps[5][:group]).to eq('*')
+    expect(deps[5][:source]).to eq('github')
+    expect(deps[5][:prod_key]).to eq('forki/FsUnit')
+    expect(deps[5][:comperator]).to eq('=')
+    expect(deps[5][:version]).to eq('7623fc13439f0e60bd05c1ed3b5f6dcb937fe468')
+
+    expect(deps[6][:group]).to eq('*')
+    expect(deps[6][:source]).to eq('git')
+    expect(deps[6][:prod_key]).to eq('https://github.com/fsprojects/Paket.git')
+    expect(deps[6][:comperator]).to eq('*')
+    expect(deps[6][:version]).to eq('')
+
+    expect(deps[7][:group]).to eq('*')
+    expect(deps[7][:source]).to eq('git')
+    expect(deps[7][:prod_key]).to eq('http://github.com/forki/AskMe.git')
+    expect(deps[7][:comperator]).to eq('=')
+    expect(deps[7][:version]).to eq('97ee5ae7074bdb414a3e5dd7d2f2d752547d0542')
+
+    expect(deps[8][:group]).to eq('*')
+    expect(deps[8][:source]).to eq('git')
+    expect(deps[8][:prod_key]).to eq('https://github.com/fsprojects/Paket2.git')
+    expect(deps[8][:comperator]).to eq('>=')
+    expect(deps[8][:version]).to eq('1.0')
+
+    expect(deps[9][:group]).to eq('*')
+    expect(deps[9][:source]).to eq('gist')
+    expect(deps[9][:prod_key]).to eq('Thorium/1972349')
+    expect(deps[9][:comperator]).to eq('*')
+    expect(deps[9][:version]).to eq('')
+
+    expect(deps[10][:group]).to eq('*')
+    expect(deps[10][:source]).to eq('http')
+    expect(deps[10][:prod_key]).to eq('http://www.fssnip.net/1n')
+    expect(deps[10][:comperator]).to eq('*')
+    expect(deps[10][:version]).to eq('')
+
+    expect(deps[11][:group]).to eq('Test')
+    expect(deps[11][:source]).to eq('nuget')
+    expect(deps[11][:prod_key]).to eq('NUnit.Runners')
+    expect(deps[11][:comperator]).to eq('*')
+    expect(deps[11][:version]).to eq('')
+
+    expect(deps[12][:group]).to eq('Test')
+    expect(deps[12][:source]).to eq('nuget')
+    expect(deps[12][:prod_key]).to eq('NUnit')
+    expect(deps[12][:comperator]).to eq('*')
+    expect(deps[12][:version]).to eq('')
+
     end
   end
 end
