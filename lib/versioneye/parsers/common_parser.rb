4d5b6fd26a5f8a8a2c9b696486a28ccc5c9a705c
Timo Sulg <timgluz@gmail.com>
2016-08-03 11:09:54 +0200

    fix: Ruby JSON module failed to parse files that included UTF spaces or non-space characters;

--- lib/versioneye/parsers/common_parser.rb
+++ lib/versioneye/parsers/common_parser.rb
@@ -13,11 +13,40 @@ class CommonParser
   end
 
   def from_json(json_doc)
+    json_doc = json_doc.force_encoding(Encoding::UTF_8).strip
+    json_doc = clean_spaces(json_doc) #replace non-ascii spaces with ascii spaces
     JSON.parse(json_doc, {symbolize_names: true})
   rescue => e
     log.error "from_json: failed to parse #{json_doc}"
     log.error e.backtrace.join('\n')
     return nil
+  end
+
+
+  SPECIAL_SPACES = [
+    0x00A0,                # NO-BREAK SPACE
+    0x1680,                # OGHAM SPACE MARK
+    0x180E,                # MONGOLIAN VOWEL SEPARATOR
+    (0x2000..0x200A).to_a, # EN QUAD..HAIR SPACE
+    0x2028,                # LINE SEPARATOR
+    0x2029,                # PARAGRAPH SEPARATOR
+    0x202F,                # NARROW NO-BREAK SPACE
+    0x205F,                # MEDIUM MATHEMATICAL SPACE
+    0x3000,                # IDEOGRAPHIC SPACE
+  ].flatten.collect{|e| [e].pack 'U*'}
+
+  ZERO_WIDTH = [
+    0x200B,                # ZERO WIDTH SPACE
+    0x200C,                # ZERO WIDTH NON-JOINER
+    0x200D,                # ZERO WIDTH JOINER
+    0x2060,                # WORD JOINER
+    0xFEFF,                # ZERO WIDTH NO-BREAK SPACE
+  ].flatten.collect{|e| [e].pack 'U*'}
+  
+  def clean_spaces(txt)
+    txt.gsub!(Regexp.new(ZERO_WIDTH.join("|")), "") 
+    txt.gsub!(Regexp.new(SPECIAL_SPACES.join("|") + "|\s"), " ")
+    txt
   end
 =begin
 
