85f18f3910141676a0ea97d67753c82c6578e8cf
reiz <robert.reiz.81@gmail.com>
2016-08-08 11:18:31 +0200

    Add new newsletter.

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => 'Support for .NET') do |format|
+    mail(:to => @user.email, :subject => 'Snyk Security + Kobalt Plugin for Kotlin') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
