456004973323810b4191563fb0eb5255de1a2a6f
reiz <robert.reiz.81@gmail.com>
2016-08-14 23:34:16 +0200

    Fix tes

--- lib/versioneye/services/admin_service.rb
+++ lib/versioneye/services/admin_service.rb
@@ -2,7 +2,7 @@ class AdminService < Versioneye::Service
 
   def self.create_default_admin
     admin = User.where({:username => 'admin'}).first
-    return true if admin
+    return false if admin
 
     admin = User.new({:username => 'admin', :fullname => 'admin',
       :email => 'admin@admin.com', :password => 'admin', :terms => true,
