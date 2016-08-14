b7cc00121ceacf02c0c6ee2024a6a7731545fa0e
reiz <robert.reiz.81@gmail.com>
2016-08-14 18:25:27 +0200

    Only create admin if there is no one.

--- lib/versioneye/services/admin_service.rb
+++ lib/versioneye/services/admin_service.rb
@@ -1,6 +1,9 @@
 class AdminService < Versioneye::Service
 
   def self.create_default_admin
+    admin = User.where({:username => 'admin'}).first
+    return true if admin
+
     admin = User.new({:username => 'admin', :fullname => 'admin',
       :email => 'admin@admin.com', :password => 'admin', :terms => true,
       :datenerhebung => true })
