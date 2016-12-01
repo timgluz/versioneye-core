a0c6adf3c72aa8a140cbb33cecffd98f5f04cc9b
reiz <robert.reiz.81@gmail.com>
2016-12-01 22:44:30 +0100

    BugFix for team notificaiton service.

--- lib/versioneye/services/team_notification_service.rb
+++ lib/versioneye/services/team_notification_service.rb
@@ -23,7 +23,7 @@ class TeamNotificationService  < Versioneye::Service
     return nil if team.notifications_all_disabled?
 
     projects = orga.team_projects team.ids
-    return nil if projects.nil? || !projects.empty?
+    return nil if projects.nil? || projects.empty?
 
     TeamMailer.team_notification( orga, team, projects )
   end
