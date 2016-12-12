f1a348406ecd365c730426165c723cd27b455835
reiz <robert.reiz.81@gmail.com>
2016-12-12 11:05:26 +0100

    Merge PR #53

--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '10.10.10'
+  VERSION = '10.10.11'
 end
--- scripts/runtests_local.sh
+++ scripts/runtests_local.sh
@@ -4,8 +4,8 @@ echo "Going to run all specs"
 export RAILS_ENV="test"
 echo "Rails mode: $RAILS_ENV"
 
-# rspec
-rspec spec/versioneye/models/organisation_spec.rb
+rspec
+# rspec spec/versioneye/models/organisation_spec.rb
 
 export RAILS_ENV="development"
 echo "Rails mode: $RAILS_ENV"
