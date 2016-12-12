bf867f5c1dc29ea068bfb207c8c6cce3728ee00d
reiz <robert.reiz.81@gmail.com>
2016-12-12 13:04:26 +0100

    Add new newsletter

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => 'Pull request support for GitHub + Vagrantfile') do |format|
+    mail(:to => @user.email, :subject => 'Team notifications + Improved security notifications') do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '10.10.11'
+  VERSION = '10.11.0'
 end
