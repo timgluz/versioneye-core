38715b9165c9fb4f392294dd63964736b09c6381
reiz <robert.reiz.81@gmail.com>
2016-12-27 10:42:01 +0100

    Remove unused code and add new index.

--- lib/versioneye/models/mail_track.rb
+++ lib/versioneye/models/mail_track.rb
@@ -17,14 +17,18 @@ class MailTrack < Versioneye::Model
   field :orga_id    , type: String
 
   index({ user_id: 1, template: 1 }, { name: "user_id_template_index", background: true })
+  index({ orga_id: 1, team_id: 1, template: 1, created_at: 1 }, { name: "team_noti_index", background: true })
+
 
   def self.add user_id, template_name, period, project_id = nil
     MailTrack.new(:user_id => user_id, :template => template_name, :period => period, :project_id => project_id ).save
   end
 
+
   def self.add_team template_name, orga_id = nil, team_id = nil, project_ids = nil
     MailTrack.new(:template => template_name, :orga_id => orga_id, :team_id => team_id, :project_ids => project_ids ).save
   end
+
 
   def self.send_team_email_already?(template_name, orga_id = nil, team_id = nil)
     time_ago = DateTime.now - 12.hours
@@ -34,15 +38,6 @@ class MailTrack < Versioneye::Model
                     :created_at.gt => time_ago).count > 0
   end
 
-  def self.send_already? user_id, template_name, period
-    mails = fetch_by user_id, template_name, period
-    !mails.empty?
-  end
-
-  def self.fetch_by user_id, template_name, period
-    time_ago = date_for period
-    MailTrack.where(:user_id => user_id, :template => template_name, :period => period, :created_at.gt => time_ago)
-  end
 
   def user
     User.find self.user_id
@@ -50,16 +45,6 @@ class MailTrack < Versioneye::Model
 
   def project
     Project.find self.project_id
-  end
-
-  def self.date_for period
-    if period.eql?(Project::A_PERIOD_DAILY)
-      return DateTime.now - 24.hours
-    elsif period.eql?(Project::A_PERIOD_WEEKLY)
-      return DateTime.now - 1.week
-    else
-      return DateTime.now - 1.month
-    end
   end
 
 end
--- spec/versioneye/models/mail_track_spec.rb
+++ spec/versioneye/models/mail_track_spec.rb
@@ -4,40 +4,12 @@ describe MailTrack do
 
   describe "fetch_by" do
 
-    it "finds the right one" do
-      expect( MailTrack.add '777', 'email', 'daily', '1' ).to be_truthy
+    # it "finds the right one" do
+    #   expect( MailTrack.add '777', 'email', 'daily', '1' ).to be_truthy
 
-      expect( MailTrack.fetch_by('777', 'email', 'daily').count ).to eq(1)
-      expect( MailTrack.send_already? '777', 'email', 'daily' ).to be_truthy
-    end
-    it "doesnt find because older than 24 hours" do
-      yesterday = DateTime.now - 25.hours
-      mt = MailTrack.new(:user_id => '777', :template => 'email', :period => 'daily', :project_id => '1', :created_at => yesterday)
-      expect( mt.save ).to be_truthy
-
-      expect( MailTrack.fetch_by('777', 'email', 'daily').count ).to eq(0)
-      expect( MailTrack.send_already? '777', 'email', 'daily' ).to be_falsey
-    end
-    it "doesnt exist already" do
-      expect( MailTrack.fetch_by('777', 'email', 'daily').count ).to eq(0)
-      expect( MailTrack.send_already? '777', 'email', 'daily' ).to be_falsey
-    end
-    it "exist already for weekkly" do
-      days_ago = DateTime.now - 2.days
-      mt = MailTrack.new(:user_id => '777', :template => 'email', :period => 'weekly', :project_id => '1', :created_at => days_ago)
-      expect( mt.save ).to be_truthy
-
-      expect( MailTrack.fetch_by('777', 'email', 'weekly').count ).to eq(1)
-      expect( MailTrack.send_already? '777', 'email', 'weekly' ).to be_truthy
-    end
-    it "exist already for monthly" do
-      weeks_ago = DateTime.now - 3.weeks
-      mt = MailTrack.new(:user_id => '777', :template => 'email', :period => 'monthly', :project_id => '1', :created_at => weeks_ago)
-      expect( mt.save ).to be_truthy
-
-      expect( MailTrack.fetch_by('777', 'email', 'monthly').count ).to eq(1)
-      expect( MailTrack.send_already? '777', 'email', 'monthly' ).to be_truthy
-    end
+    #   expect( MailTrack.fetch_by('777', 'email', 'daily').count ).to eq(1)
+    #   expect( MailTrack.send_already? '777', 'email', 'daily' ).to be_truthy
+    # end
 
   end
 
