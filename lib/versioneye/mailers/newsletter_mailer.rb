c9e224b135aacab72cf099955a49abd624563fa5
reiz <robert.reiz.81@gmail.com>
2017-01-24 19:10:52 +0200

    Add new newsletter

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => 'Team notifications + Improved security notifications') do |format|
+    mail(:to => @user.email, :subject => "Let's make licenses great again!") do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '11.3.7'
+  VERSION = '11.3.8'
 end
