20ba66d46ad4a82708b7c20dfcd55acb2548aa36
reiz <robert.reiz.81@gmail.com>
2017-02-06 15:02:29 +0100

    Update the newsletter.

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => "Let's make licenses great again!") do |format|
+    mail(:to => @user.email, :subject => "5 years of VersionEye") do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '11.5.0'
+  VERSION = '11.5.1'
 end
