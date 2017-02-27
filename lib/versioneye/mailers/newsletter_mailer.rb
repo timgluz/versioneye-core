a9baa986ddbcb2465534b2219c1618e245bd0c50
reiz <robert.reiz.81@gmail.com>
2017-02-27 19:51:57 +0200

    Add new newsletter

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => "5 years of VersionEye") do |format|
+    mail(:to => @user.email, :subject => "Identifying components by SHA values") do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '11.7.7'
+  VERSION = '11.7.8'
 end
