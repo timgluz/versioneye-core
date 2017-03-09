fda771946223664085efc735eeeb58680064a306
reiz <robert.reiz.81@gmail.com>
2017-03-09 14:50:57 +0100

    BugFix for duplicate filter

--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '11.7.18'
+  VERSION = '11.7.19'
 end
--- scripts/runtests_local.sh
+++ scripts/runtests_local.sh
@@ -4,7 +4,7 @@ echo "Going to run all specs"
 export RAILS_ENV="test"
 echo "Rails mode: $RAILS_ENV"
 
-rspec # spec/versioneye/parsers/package_parser_*
+rspec # spec/versioneye/models/organisation_spec.rb
 
 export RAILS_ENV="development"
 echo "Rails mode: $RAILS_ENV"
