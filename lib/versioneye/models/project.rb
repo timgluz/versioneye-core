d6aaf96d8daa50fc61476bde74163e9c76ea8496
reiz <robert.reiz.81@gmail.com>
2017-04-17 11:18:58 +0200

    Add new index for project

--- lib/versioneye/models/project.rb
+++ lib/versioneye/models/project.rb
@@ -100,6 +100,7 @@ class Project < Versioneye::Model
   index({source: 1},  { name: "source_index",  background: true})
   index({parent_id: 1}, { name: "parentid_index", background: true})
   index({parent_id: 1, temp: 1, team_ids: 1, organisation_id: 1}, { name: "project_overview_index", background: true})
+  index({organisation_id: 1},  { name: "orgaid_index",  background: true})
 
   scope :by_user   , ->(user)    { where(user_id: user[:_id].to_s) }
   scope :by_user_id, ->(user_id) { where(user_id: user_id.to_s) }
