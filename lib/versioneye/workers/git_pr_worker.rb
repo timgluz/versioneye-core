57c51cb2e160b5094f5ef67b3c386558614b654e
reiz <robert.reiz.81@gmail.com>
2017-04-18 10:23:03 +0200

    Add worker for GitHub pull request

---
+++ lib/versioneye/workers/git_pr_worker.rb
@@ -0,0 +1,45 @@
+require 'benchmark'
+require 'dalli'
+
+class GitPrWorker < Worker
+
+  A_TASK_TTL = 60 # 60 seconds = 1 minute
+
+  def work
+    connection = get_connection
+    connection.start
+    channel = connection.create_channel
+    channel.prefetch(1)
+    queue   = channel.queue("git_pr_processor", :durable => true)
+
+    multi_log " [*] GitPrWorker waiting for messages in #{queue.name}. To exit press CTRL+C"
+
+    begin
+      queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
+        multi_log " [x] GitPrWorker received #{body}"
+        handle_pr body
+        channel.ack(delivery_info.delivery_tag)
+        multi_log " [x] GitPrWorker job done #{body}"
+      end
+    rescue => e
+      log.error "ERROR in GitPrWorker: #{e.message}"
+      log.error e.backtrace.join("\n")
+      connection.close
+    end
+  end
+
+  private
+
+    def handle_pr msg
+      sps         = message.split(":::")
+      repo_name   = sps[0]
+      branch      = sps[1]
+      commits_url = sps[2]
+      pr_nr       = sps[3]
+      GithubPullRequestService.process repo_name, branch, commits_url, pr_nr
+    rescue => e
+      log.error "ERROR in GitPrWorker! Input: #{message} Output: #{e.message}"
+      log.error e.backtrace.join("\n")
+    end
+
+end
