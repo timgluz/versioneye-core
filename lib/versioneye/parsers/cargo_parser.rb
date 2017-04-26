979fbcaad3febd767180f0ab70a2054bb7408c07
Timo Sulg <timgluz@gmail.com>
2017-04-26 16:21:35 +0200

    add support for caret, tilde and wildcard selectors;

--- lib/versioneye/parsers/cargo_parser.rb
+++ lib/versioneye/parsers/cargo_parser.rb
@@ -6,11 +6,13 @@ require 'semverly'
 # uses semver implementation: https://github.com/steveklabnik/semver-parser
 
 class CargoParser < CommonParser
+  FIXNUM_MAX = (2**(0.size * 8 -2) -1)
 
   def parse_requested_version(version, dependency, product)
     version = version.to_s.strip
-    if version.empty?
+    if version.empty? or ['*', 'X', 'x'].include?(version)
       log.error "#{product} version label is missing."
+      dependency[:version_label] = version
       update_requested_with_current(dependency, product)
       return dependency
     end
@@ -61,9 +63,200 @@ class CargoParser < CommonParser
       dependency[:version_requested]  = newest_version.to_s
       dependency[:version_label]      = version_label
       dependency[:comperator]         = '>'
+
+    elsif version[0] == '^' or is_semver(version)
+      version_label = version.gsub(/\^\s*/, '').to_s.strip
+      dependency[:version_requested]  = newest_caret_version(product.versions, version_label)
+      dependency[:version_label]      = version_label
+      dependency[:comperator]         = '^'
+
+    elsif version[0] == '~'
+      version_label = version.gsub(/\~\s*/, '').to_s.strip
+      dependency[:version_requested]  = newest_tilde_version(product.versions, version_label)
+      dependency[:version_label]      = version_label
+      dependency[:comperator]         = '~'
+    elsif version =~ /\.[\*|x|X]/
+      dependency[:version_requested]  = newest_wildcard_version(product.versions, version)
+      dependency[:version_label]      = version
+      dependency[:comperator]         = '*'
     end
 
     dependency
   end
 
+  def is_semver(version_label)
+    !SemVer.parse(version_label.to_s).nil?
+  end
+
+  def newest_tilde_version(versions, version_label)
+    lower_border = tilde_lower_border version_label
+    upper_border = tilde_upper_border version_label
+
+    if lower_border.nil? or upper_border.nil?
+      return version_label
+    end
+
+    greater_than = VersionService.greater_than_or_equal versions, lower_border, true
+    newest_version = VersionService.smaller_than(greater_than, upper_border)
+    if newest_version
+      newest_version
+    else
+      version_label
+    end
+  end
+
+  def newest_caret_version(versions, version_label)
+      lower_border = caret_lower_border version_label
+      upper_border = caret_upper_border version_label
+      if lower_border.nil? or upper_border.nil?
+        return version_label
+      end
+
+      greater_than = VersionService.greater_than_or_equal versions, lower_border, true
+      newest_version = VersionService.smaller_than(greater_than, upper_border)
+      if newest_version
+        newest_version
+      else
+        version_label
+      end
+  end
+
+  def newest_wildcard_version(versions, version_label)
+    lower_border = wildcard_lower_border version_label
+    upper_border = wildcard_upper_border version_label
+    if lower_border.nil? or upper_border.nil?
+      return version_label
+    end
+
+    greater_than = VersionService.greater_than_or_equal versions, lower_border, true
+    newest_version = VersionService.smaller_than(greater_than, upper_border)
+    if newest_version
+      newest_version
+    else
+      version_label
+    end
+  end
+
+  # turns caret semver selector into lower version
+  # returns:
+  # nil - failed to parse semver
+  # string - lower range of caret version
+  def caret_lower_border(version_label)
+    lower_ver = SemVer.parse version_label
+    return nil if lower_ver.nil?
+    #remove metdata details
+    lower_ver.metadata = nil
+    lower_ver.prerelease = nil
+
+    lower_ver.to_s
+  end
+
+  # calculates upper version of caret semver
+  # it increments most left-most non-zero value
+  # returns:
+  # nil - failed to parse semver
+  # string - upper range of caret version
+  def caret_upper_border(version_label)
+    upper_ver = SemVer.parse version_label
+    return nil if upper_ver.nil?
+
+    #remove metdata and pre-release details
+    upper_ver.metadata = nil
+    upper_ver.prerelease = nil
+
+    #increase left-most non-zero version item
+    if upper_ver.major != 0
+      upper_ver.major += 1
+      upper_ver.minor = 0
+      upper_ver.patch = 0
+    elsif upper_ver.major == 0 and upper_ver.minor != 0
+      upper_ver.minor += 1
+      upper_ver.patch = 0
+    else
+      if upper_ver.patch != 0
+        upper_ver.patch += 1
+      else
+        upper_ver.major += 1 #when version is '0.0.0'
+      end
+    end
+
+    upper_ver.to_s
+  end
+
+  # finds the lower version for tilde selector
+  # returns:
+  #   nil - failed to parse semver
+  #   string - lower border of tilde selector
+  def tilde_lower_border(version_label)
+    lower_ver =  SemVer.parse version_label
+    return nil if lower_ver.nil?
+
+    #remove metdata details
+    lower_ver.metadata = nil
+    lower_ver.prerelease = nil
+
+    lower_ver.to_s
+  end
+
+  # finds the upper version of the tilde selector
+  # it allows only changes in patch => has at least major, minor part
+  # it allows only changes in minor => has only major part
+  # returns:
+  #   nil - failed to parse semver
+  #   string - upper border of tilde selector
+  def tilde_upper_border(version_label)
+    upper_ver = SemVer.parse version_label
+    return nil if upper_ver.nil?
+
+    #remove metadata and prerelease details
+    upper_ver.metadata = nil
+    upper_ver.prerelease = nil
+
+    if version_label =~ /^\d+\.\d+/
+      upper_ver.minor += 1
+      upper_ver.patch = 0
+    else
+      upper_ver.major += 1
+      upper_ver.minor = 0
+      upper_ver.patch = 0
+    end
+
+    upper_ver.to_s
+  end
+
+  # finds the lower version of the wildcard selector
+  # replaces each * with 0
+  def wildcard_lower_border(version_label)
+    version = version_label.to_s.gsub(/\*+/, '0').to_s
+    lower_ver = SemVer.parse version
+    return nil if lower_ver.nil?
+
+    #remove metadata and prerelease details
+    lower_ver.metadata = nil
+    lower_ver.prerelease = nil
+
+    lower_ver.to_s
+  end
+
+  #finds the highest version of the wildcard selector
+  # replaces each * with 0 and increments rightmost non-zero value
+  def wildcard_upper_border(version_label)
+    version = version_label.to_s.gsub(/\*+/, '0').to_s
+    upper_ver = SemVer.parse version
+    return nil if upper_ver.nil?
+
+    #remove metadata and prerelease details
+    upper_ver.metadata = nil
+    upper_ver.prerelease = nil
+
+    if version_label =~ /^\d+\.\d+\.[\*|x|X]/
+      upper_ver.minor += 1
+    elsif version_label =~ /^\d+\.[\*|x|X]/
+      upper_ver.major += 1
+    elsif version_label =~ /\*|x|X/
+      upper_ver.major = FIXNUM_MAX #earliest rules should match this aka use latest version rule
+    end
+
+    upper_ver.to_s
+  end
 end
--- spec/versioneye/parsers/cargo_parser_spec.rb
+++ spec/versioneye/parsers/cargo_parser_spec.rb
@@ -22,6 +22,82 @@ describe CargoParser do
     )
   }
 
+  context "is_semver" do
+    it "returns truthy for version without selectors" do
+      expect( parser.is_semver('1') ).to be_truthy
+      expect( parser.is_semver('1.2')).to be_truthy
+      expect( parser.is_semver('1.2.3')).to be_truthy
+    end
+
+    it "returns falsey for version with selectors" do
+      expect( parser.is_semver('>1')).to be_falsey
+      expect( parser.is_semver('*')).to be_falsey
+      expect( parser.is_semver('^ 1.0.0')).to be_falsey
+    end
+  end
+
+  context "caret_lower_border" do
+    it "returns correct semver strings" do
+      expect( parser.caret_lower_border("1.2.3")).to eq('1.2.3')
+      expect( parser.caret_lower_border("1.2.3-alpha")).to eq('1.2.3')
+      expect( parser.caret_lower_border("1.2.3-beta")).to eq('1.2.3')
+
+      expect( parser.caret_lower_border('1.2') ).to eq('1.2.0')
+      expect( parser.caret_lower_border('0.2.3') ).to eq('0.2.3')
+      expect( parser.caret_lower_border('0.0.3') ).to eq('0.0.3')
+      expect( parser.caret_lower_border('0.0')).to eq('0.0.0')
+      expect( parser.caret_lower_border('0')).to eq('0.0.0')
+    end
+  end
+
+  context "caret_upper_border" do
+    it "returns correct upper lever semver" do
+      expect( parser.caret_upper_border('1.2.3') ).to eq('2.0.0')
+      expect( parser.caret_upper_border('1.2') ).to eq('2.0.0')
+      expect( parser.caret_upper_border('1') ).to eq('2.0.0')
+      expect( parser.caret_upper_border('0.2.3')).to eq('0.3.0')
+      expect( parser.caret_upper_border('0.0.3')).to eq('0.0.4')
+      expect( parser.caret_upper_border('0.0.0')).to eq('1.0.0')
+    end
+  end
+
+  context "tilde_lower_border" do
+    it "returns correct lower border" do
+      expect( parser.tilde_lower_border('1.2.3')).to eq('1.2.3')
+      expect( parser.tilde_lower_border('1.2')).to eq('1.2.0')
+      expect( parser.tilde_lower_border('1')).to eq('1.0.0')
+    end
+  end
+
+  context "tilde_upper_border" do
+    it "returns correct upper version" do
+      expect( parser.tilde_upper_border('1.2.3')).to eq('1.3.0')
+      expect( parser.tilde_upper_border('1.2')).to eq('1.3.0')
+      expect( parser.tilde_upper_border('1.0')).to eq('1.1.0')
+      expect( parser.tilde_upper_border('1')).to eq('2.0.0')
+      expect( parser.tilde_upper_border('0')).to eq('1.0.0')
+    end
+  end
+
+  context "wildcard_lower_border" do
+    it "returns correct lower border" do
+      expect( parser.wildcard_lower_border('1.2.*')).to eq('1.2.0')
+      expect( parser.wildcard_lower_border('1.*')).to eq('1.0.0')
+      expect( parser.wildcard_lower_border('*')).to eq('0.0.0')
+    end
+  end
+
+  context "wildcard_upper_border" do
+    it "returns correct upper borders" do
+      expect( parser.wildcard_upper_border('1.2.*')).to eq('1.3.0')
+      expect( parser.wildcard_upper_border('1.*')).to eq('2.0.0')
+      expect( parser.wildcard_upper_border('0.6.*')).to eq('0.7.0')
+      expect( parser.wildcard_upper_border('0.*')).to eq('1.0.0')
+      expect( parser.wildcard_upper_border('*')).to eq("#{CargoParser::FIXNUM_MAX}.0.0")
+    end
+  end
+
+
   context "parse_requested_version" do
     before do
       product1.versions << Version.new(version: '0.6.0')
@@ -34,7 +110,16 @@ describe CargoParser do
 
       expect(dep).not_to be_nil
       expect(dep[:version_requested]).to eq(product1[:version])
-      expect(dep[:version_label]).to be_nil
+      expect(dep[:version_label]).to eq('')
+      expect(dep[:comperator]).to eq('=')
+    end
+
+    it "uses latest product version if version label is *, x, X" do
+      dep = parser.parse_requested_version("*", dep1, product1)
+
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq(product1[:version])
+      expect(dep[:version_label]).to eq('*')
       expect(dep[:comperator]).to eq('=')
     end
 
@@ -101,6 +186,73 @@ describe CargoParser do
       expect(dep[:version_requested]).to eq('0.7.0')
       expect(dep[:version_label]).to eq('0.6.2')
       expect(dep[:comperator]).to eq('>')
+    end
+
+    it "parses correctly caret selectors" do
+      product1.versions << Version.new(version: '0.7.0')
+      product1.versions << Version.new(version: '1.0.0')
+      product1.save
+
+      dep = parser.parse_requested_version '^0.6', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.6.2')
+      expect(dep[:version_label]).to eq('0.6')
+      expect(dep[:comperator]).to eq('^')
+
+      dep = parser.parse_requested_version '^0', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.7.0')
+      expect(dep[:version_label]).to eq('0')
+      expect(dep[:comperator]).to eq('^')
+
+      #NB! versions without any specifiers are classified as caret selector
+      dep = parser.parse_requested_version '0.6', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.6.2')
+      expect(dep[:version_label]).to eq('0.6')
+      expect(dep[:comperator]).to eq('^')
+    end
+
+    it "parses correctly tilde specifiers" do
+      product1.versions << Version.new(version: '0.7.0')
+      product1.versions << Version.new(version: '1.0.0')
+      product1.save
+
+      dep = parser.parse_requested_version '~0.6', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.6.2')
+      expect(dep[:version_label]).to eq('0.6')
+      expect(dep[:comperator]).to eq('~')
+
+      dep = parser.parse_requested_version '~0', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.7.0')
+      expect(dep[:version_label]).to eq('0')
+      expect(dep[:comperator]).to eq('~')
+    end
+
+    it "parses correctly wildcard selectors" do
+      product1.versions << Version.new(version: '0.7.0')
+      product1.versions << Version.new(version: '1.0.0')
+      product1.save
+
+      dep = parser.parse_requested_version '0.6.*', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.6.2')
+      expect(dep[:version_label]).to eq('0.6.*')
+      expect(dep[:comperator]).to eq('*')
+
+      dep = parser.parse_requested_version '0.*', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('0.7.0')
+      expect(dep[:version_label]).to eq('0.*')
+      expect(dep[:comperator]).to eq('*')
+
+      dep = parser.parse_requested_version '1.*', dep1, product1
+      expect(dep).not_to be_nil
+      expect(dep[:version_requested]).to eq('1.0.0')
+      expect(dep[:version_label]).to eq('1.*')
+      expect(dep[:comperator]).to eq('*')
     end
   end
 end
