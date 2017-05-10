3a18f533f7d98d1db3ea48af226860ec7e12f39c
reiz <robert.reiz.81@gmail.com>
2017-05-10 10:39:50 +0200

    Update newsletter.

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => "Support for Rust") do |format|
+    mail(:to => @user.email, :subject => "Support for Rust and a new project view") do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
--- lib/versioneye/version.rb
+++ lib/versioneye/version.rb
@@ -1,3 +1,3 @@
 module Versioneye
-  VERSION = '11.11.1'
+  VERSION = '11.11.2'
 end
