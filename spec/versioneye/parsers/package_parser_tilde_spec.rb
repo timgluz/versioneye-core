ef780fdfac42f7801d8630bf0cbc422821933405
Timo Sulg <timgluz@gmail.com>
2017-06-23 13:32:55 +0200

    remove deprecation warnings from package_parser_tilde_spec

--- spec/versioneye/parsers/package_parser_tilde_spec.rb
+++ spec/versioneye/parsers/package_parser_tilde_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe PackageParser do
+  let(:parser){ PackageParser.new }
+  let(:test_file_url){ 'https://s3.amazonaws.com/veye_test_env/npm_1/package.json'}
 
   describe "parse" do
 
     it "parse from https the file correctly" do
-      parser = PackageParser.new
-      project = parser.parse('https://s3.amazonaws.com/veye_test_env/npm_1/package.json')
-      project.should_not be_nil
+      project = parser.parse test_file_url
+      expect( project ).not_to be_nil
     end
 
     it "parse from http the file correctly" do
@@ -17,42 +18,41 @@ describe PackageParser do
       product3 = create_product('gcloud'   , 'gcloud'   , '0.2.0', ['0.20.0' , '0.21.0', '0.22.0', '0.23.0', '0.24.0', '0.24.1', '0.25.0', '0.25.1', '0.26.0', '0.26.1' ] )
       product4 = create_product('express'  , 'express'   , '4.13.3', ['4.0.0' , '4.1.0', '4.13.3' ] )
 
-      parser = PackageParser.new
-      project = parser.parse('https://s3.amazonaws.com/veye_test_env/npm_1/package.json')
-      project.should_not be_nil
-      project.dependencies.size.should eql(4)
+      project = parser.parse test_file_url
+      expect( project ).not_to be_nil
+      expect( project.dependencies.size ).to eql(4)
 
       dep_1 = project.dependencies[0]
-      dep_1.name.should eql('eslint')
-      dep_1.version_label.should eql('^1.0.0')
-      dep_1.version_requested.should eql('1.1.0')
-      dep_1.version_current.should eql('1.1.0')
-      dep_1.comperator.should eql('^')
-      dep_1.outdated?().should be_falsey
+      expect( dep_1.name ).to               eql(product1[:name])
+      expect( dep_1.version_label ).to      eql('^1.0.0')
+      expect( dep_1.version_requested ).to  eql('1.1.0')
+      expect( dep_1.version_current ).to    eql('1.1.0')
+      expect( dep_1.comperator ).to         eql('^')
+      expect( dep_1.outdated? ).to          be_falsey
 
       dep_2 = project.dependencies[1]
-      dep_2.name.should eql('inquirer')
-      dep_2.version_label.should eql('^0.8.0')
-      dep_2.version_requested.should eql('0.8.5')
-      dep_2.version_current.should eql('0.9.0')
-      dep_2.comperator.should eql('^')
-      dep_2.outdated?().should be_truthy
+      expect( dep_2.name ).to               eql(product2[:name])
+      expect( dep_2.version_label ).to      eql('^0.8.0')
+      expect( dep_2.version_requested ).to  eql('0.8.5')
+      expect( dep_2.version_current ).to    eql('0.9.0')
+      expect( dep_2.comperator ).to         eql('^')
+      expect( dep_2.outdated? ).to          be_truthy
 
       dep_3 = project.dependencies[2]
-      dep_3.name.should eql('gcloud')
-      dep_3.version_label.should eql('^0.23')
-      dep_3.version_requested.should eql('0.23.0')
-      dep_3.version_current.should eql('0.26.1')
-      dep_3.comperator.should eql('^')
-      dep_3.outdated?().should be_truthy
+      expect( dep_3.name ).to               eql(product3[:name])
+      expect( dep_3.version_label ).to      eql('^0.23')
+      expect( dep_3.version_requested ).to  eql('0.23.0')
+      expect( dep_3.version_current ).to    eql('0.26.1')
+      expect( dep_3.comperator ).to         eql('^')
+      expect( dep_3.outdated? ).to          be_truthy
 
       dep_4 = project.dependencies[3]
-      dep_4.name.should eql('express')
-      dep_4.version_label.should eql('~4.*')
-      dep_4.version_requested.should eql('4.13.3')
-      dep_4.version_current.should eql('4.13.3')
-      dep_4.comperator.should eql('~')
-      dep_4.outdated?().should be_falsey
+      expect( dep_4.name ).to               eql(product4[:name])
+      expect( dep_4.version_label ).to      eql('~4.*')
+      expect( dep_4.version_requested ).to  eql('4.13.3')
+      expect( dep_4.version_current ).to    eql('4.13.3')
+      expect( dep_4.comperator ).to         eql('~')
+      expect( dep_4.outdated? ).to          be_falsey
     end
 
   end
