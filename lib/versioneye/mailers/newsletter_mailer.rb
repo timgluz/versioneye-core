0393102da54550206b3714e8661df5b53007d63c
reiz <robert.reiz.81@gmail.com>
2017-08-23 11:00:43 +0200

    Update Newsletter

--- lib/versioneye/mailers/newsletter_mailer.rb
+++ lib/versioneye/mailers/newsletter_mailer.rb
@@ -4,7 +4,7 @@ class NewsletterMailer < SuperMailer
   def newsletter_new_features_email( user )
     @user = user
     @newsletter = "newsletter_features"
-    mail(:to => @user.email, :subject => "Perl language support & Rust Security!") do |format|
+    mail(:to => @user.email, :subject => "Security Vulnerabilities for JavaScript") do |format|
       format.html{ render layout: 'email_html_layout' }
     end
   end
